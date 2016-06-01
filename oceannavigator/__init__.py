ea049d5c5fd6ea99044dd69e10cea0225ef20479
Geoff Holden <geoff@geoffholden.com>
2016-06-01 15:07:50 -0230

    Move config to init.py

--- oceannavigator/__init__.py
+++ oceannavigator/__init__.py
@@ -3,5 +3,7 @@
 from flask import Flask, Response, request
 app = Flask(__name__, static_url_path='', static_folder='static')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
+app.config.from_pyfile('oceannavigator.cfg', silent=False)
+app.config.from_envvar('OCEANNAVIGATOR_SETTINGS', silent=True)
 
 import oceannavigator.views
--- runserver.py
+++ runserver.py
@@ -2,11 +2,4 @@
 
 from oceannavigator import app
 
-# app.config.update(
-#     DEBUG=True,
-#     THREDDS_SERVER="http://localhost:8080/thredds/",
-#     CACHE_DIR="/tmp"
-# )
-app.config.from_pyfile('oceannavigator.cfg', silent=False)
-app.config.from_envvar('OCEANNAVIGATOR_SETTINGS', silent=True)
 app.run(host='0.0.0.0', port=5050)
