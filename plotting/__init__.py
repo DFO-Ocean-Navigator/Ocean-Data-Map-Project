58655a9f132bd3cf4bf2bdf483a7d68d20ab46ee
Geoff Holden <geoff@geoffholden.com>
2017-03-10 11:35:52 -0330

    Ignore matplotlib warning about font cache

--- plotting/__init__.py
+++ plotting/__init__.py
@@ -1,3 +1,4 @@
+import warnings
 import matplotlib
 
 # Force the non-interactive PNG backend
@@ -5,3 +6,7 @@ matplotlib.use('AGG')
 
 # Use a font that supports unicode glyphs
 matplotlib.rc('font', family='DejaVu Sans')
+
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore', UserWarning)
+    import matplotlib.pyplot
