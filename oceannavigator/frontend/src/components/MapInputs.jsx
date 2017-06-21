72bc5458e8a1bf1b965a17742c4f99dd85afeb4f
Nabil M <nmiri127@gmail.com>
2017-06-21 12:31:33 -0230

    Switched to Bootstrap checkbox
    * Also grouped together settings in sidebar.

--- oceannavigator/frontend/src/components/MapInputs.jsx
+++ oceannavigator/frontend/src/components/MapInputs.jsx
@@ -3,6 +3,8 @@ import ComboBox from "./ComboBox.jsx";
 import TimePicker from "./TimePicker.jsx";
 import Range from "./Range.jsx";
 import SelectBox from "./SelectBox.jsx";
+import {Panel} from "react-bootstrap";
+
 var i18n = require("../i18n.js");
 
 class MapInputs extends React.Component {
@@ -17,117 +19,129 @@ class MapInputs extends React.Component {
     _("Variable Range");
     _("Show Bathymetry Contours");
 
-    const className = this.props.state.sidebarOpen ? 'MapInputs open' : 'MapInputs';
+    const className = this.props.state.sidebarOpen ? "MapInputs open" : "MapInputs";
 
     return (
       <div className={className}>
-        <ComboBox
-          key='projection'
-          id='projection'
-          state={this.props.state.projection}
-          onUpdate={this.props.changeHandler}
-          data={[
-            {id: "EPSG:3857", value: _("Global")},
-            {id: "EPSG:32661", value: _("Arctic")},
-            {id: "EPSG:3031", value: _("Antarctic")},
-          ]}
-          title={_("Projection")}
-        />
-        <ComboBox
-          key='basemap'
-          id='basemap'
-          state={this.props.state.basemap}
-          onUpdate={this.props.changeHandler}
-          data={[
-            {
-              id: "topo",
-              value: _("ETOPO1 Topography"),
-              attribution: "Topographical Data from ETOPO1 1 Arc-Minute Global Relief Model. NCEI, NESDIR, NOAA, U.S. Department of Commerce"
-            },
-            {
-              id: "ocean",
-              value: _("Esri Ocean Basemap"),
-              attribution: "Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri"
-            },
-            {
-              id: "world",
-              value: _("Esri World Imagery"),
-              attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
-            },
-          ]}
-          title={_("Basemap")}
-        />
-        <SelectBox
-          key='bathymetry'
-          id='bathymetry'
-          state={this.props.state.bathymetry}
-          onUpdate={this.props.changeHandler}
-          title={_("Show Bathymetry Contours")}
-        />
-        <ComboBox
-          key='dataset'
-          id='dataset'
-          state={this.props.state.dataset}
-          def={"defaults.dataset"}
-          onUpdate={this.props.changeHandler}
-          url='/api/datasets/'
-          title={_("Dataset")}></ComboBox>
-        <ComboBox
-          key='variable'
-          id='variable'
-          state={this.props.state.variable}
-          def={"defaults.dataset"}
-          onUpdate={this.props.changeHandler}
-          url={"/api/variables/?vectors&dataset=" +
-            this.props.state.dataset +
-            "&anom"
-          }
-          title={_("Variable")}
-        ><h1>{_("Variable")}</h1></ComboBox>
-        <ComboBox
-          key='depth'
-          id='depth'
-          state={this.props.state.depth}
-          def={"defaults[this.state.type].depth"}
-          onUpdate={this.props.changeHandler}
-          url={"/api/depth/?variable=" +
-            this.props.state.variable +
-            "&dataset=" +
-            this.props.state.dataset
-          }
-          title={_("Depth")}
-        ></ComboBox>
-        <TimePicker
-          key='time'
-          id='time'
-          state={this.props.state.time}
-          def={"defaults[this.state.type].time"}
-          quantum={this.props.state.dataset_quantum}
-          onUpdate={this.props.changeHandler}
-          url={"/api/timestamps/?dataset=" +
-            this.props.state.dataset +
-            "&quantum=" +
-            this.props.state.dataset_quantum
-          }
-          title={_("Time")}
-        ></TimePicker>
-        <Range
-          key='scale'
-          id='scale'
-          state={this.props.state.scale}
-          def=''
-          onUpdate={this.props.changeHandler}
-          title={_("Variable Range")}
-          autourl={"/api/range/" +
-            this.props.state.dataset + "/" +
-            this.props.state.projection + "/" +
-            this.props.state.extent.join(",") + "/" +
-            this.props.state.depth + "/" +
-            this.props.state.time + "/" +
-            this.props.state.variable + ".json"
-          }
-          default_scale={this.props.state.variable_scale}
-        ></Range>
+      <Panel 
+        header="Global Map Settings"  
+        expanded 
+        bsStyle='primary' 
+      >
+          <ComboBox
+            key='projection'
+            id='projection'
+            state={this.props.state.projection}
+            onUpdate={this.props.changeHandler}
+            data={[
+              { id: "EPSG:3857", value: _("Global") },
+              { id: "EPSG:32661", value: _("Arctic") },
+              { id: "EPSG:3031", value: _("Antarctic") },
+            ]}
+            title={_("Projection")}
+          />
+          <ComboBox
+            key='basemap'
+            id='basemap'
+            state={this.props.state.basemap}
+            onUpdate={this.props.changeHandler}
+            data={[
+              {
+                id: "topo",
+                value: _("ETOPO1 Topography"),
+                attribution: "Topographical Data from ETOPO1 1 Arc-Minute Global Relief Model. NCEI, NESDIR, NOAA, U.S. Department of Commerce"
+              },
+              {
+                id: "ocean",
+                value: _("Esri Ocean Basemap"),
+                attribution: "Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri"
+              },
+              {
+                id: "world",
+                value: _("Esri World Imagery"),
+                attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+              },
+            ]}
+            title={_("Basemap")}
+          />
+          <SelectBox
+            key='bathymetry'
+            id='bathymetry'
+            state={this.props.state.bathymetry}
+            onUpdate={this.props.changeHandler}
+            title={_("Show Bathymetry Contours")}
+          />
+        </Panel>
+        <Panel 
+          header="Primary View"  
+          expanded 
+          bsStyle='primary' 
+        >
+          <ComboBox
+            key='dataset'
+            id='dataset'
+            state={this.props.state.dataset}
+            def={"defaults.dataset"}
+            onUpdate={this.props.changeHandler}
+            url='/api/datasets/'
+            title={_("Dataset")}></ComboBox>
+          <ComboBox
+            key='variable'
+            id='variable'
+            state={this.props.state.variable}
+            def={"defaults.dataset"}
+            onUpdate={this.props.changeHandler}
+            url={"/api/variables/?vectors&dataset=" +
+              this.props.state.dataset +
+              "&anom"
+            }
+            title={_("Variable")}
+          ><h1>{_("Variable")}</h1></ComboBox>
+          <ComboBox
+            key='depth'
+            id='depth'
+            state={this.props.state.depth}
+            def={"defaults[this.state.type].depth"}
+            onUpdate={this.props.changeHandler}
+            url={"/api/depth/?variable=" +
+              this.props.state.variable +
+              "&dataset=" +
+              this.props.state.dataset
+            }
+            title={_("Depth")}
+          ></ComboBox>
+          <TimePicker
+            key='time'
+            id='time'
+            state={this.props.state.time}
+            def={"defaults[this.state.type].time"}
+            quantum={this.props.state.dataset_quantum}
+            onUpdate={this.props.changeHandler}
+            url={"/api/timestamps/?dataset=" +
+              this.props.state.dataset +
+              "&quantum=" +
+              this.props.state.dataset_quantum
+            }
+            title={_("Time")}
+          ></TimePicker>
+          <Range
+            key='scale'
+            id='scale'
+            state={this.props.state.scale}
+            def=''
+            onUpdate={this.props.changeHandler}
+            title={_("Variable Range")}
+            autourl={"/api/range/" +
+              this.props.state.dataset + "/" +
+              this.props.state.projection + "/" +
+              this.props.state.extent.join(",") + "/" +
+              this.props.state.depth + "/" +
+              this.props.state.time + "/" +
+              this.props.state.variable + ".json"
+            }
+            default_scale={this.props.state.variable_scale}
+          ></Range>
+        </Panel>
       </div>
     );
   }
