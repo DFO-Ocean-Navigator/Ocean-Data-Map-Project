d28df84c1e4bf6c8dc3dfe726e80508a17d527b7
Nabil M <nmiri127@gmail.com>
2017-06-21 11:33:03 -0230

    Grouped sidebar elements together.
    * Using Bootstrap panels, the sidebar has been divided into 3 main sections, Global Settings, Primary/Left View, Right View.
    * Variable Range is left unchanged since it is to be refactored into the DatasetSelector component.
    * Reduced sidebar padding.

--- oceannavigator/frontend/src/components/MapInputs.jsx
+++ oceannavigator/frontend/src/components/MapInputs.jsx
@@ -3,6 +3,8 @@ import ComboBox from "./ComboBox.jsx";
 import Range from "./Range.jsx";
 import SelectBox from "./SelectBox.jsx";
 import DatasetSelector from "./DatasetSelector.jsx";
+import {PanelGroup, Panel} from "react-bootstrap";
+
 var i18n = require("../i18n.js");
 
 class MapInputs extends React.Component {
@@ -13,75 +15,93 @@ class MapInputs extends React.Component {
     _("Variable Range");
     _("Show Bathymetry Contours");
 
-    const className = this.props.state.sidebarOpen ? 'MapInputs open' : 'MapInputs';
+    const className = this.props.state.sidebarOpen ? "MapInputs open" : "MapInputs";
 
     return (
       <div className={className}>
-        <ComboBox
-          key='projection'
-          id='projection'
-          state={this.props.state.projection}
-          onUpdate={this.props.changeHandler}
-          data={[
-            {id: "EPSG:3857", value: _("Global")},
-            {id: "EPSG:32661", value: _("Arctic")},
-            {id: "EPSG:3031", value: _("Antarctic")},
-          ]}
-          title={_("Projection")}
-        />
-        <ComboBox
-          key='basemap'
-          id='basemap'
-          state={this.props.state.basemap}
-          onUpdate={this.props.changeHandler}
-          data={[
-            {
-              id: "topo",
-              value: _("ETOPO1 Topography"),
-              attribution: "Topographical Data from ETOPO1 1 Arc-Minute Global Relief Model. NCEI, NESDIR, NOAA, U.S. Department of Commerce"
-            },
-            {
-              id: "ocean",
-              value: _("Esri Ocean Basemap"),
-              attribution: "Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri"
-            },
-            {
-              id: "world",
-              value: _("Esri World Imagery"),
-              attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
-            },
-          ]}
-          title={_("Basemap")}
-        />
-        <SelectBox
-          key='bathymetry'
-          id='bathymetry'
-          state={this.props.state.bathymetry}
-          onUpdate={this.props.changeHandler}
-          title={_("Show Bathymetry Contours")}
-        />
-        <DatasetSelector
-          key='dataset_0'
-          id='dataset_0'
-          state={this.props.state}
-          onUpdate={this.props.changeHandler}
-          depth={true}
-        />
-        <SelectBox
-          key='dataset_compare'
-          id='dataset_compare'
-          state={this.props.state.dataset_compare}
-          onUpdate={this.props.changeHandler}
-          title={_("Compare Dataset")}
-        />
-        <div style={{"display": this.props.state.dataset_compare ? "block" : "none"}}>
+        <Panel
+          header="Global Map Settings" 
+          expanded
+          bsStyle='primary'
+        >
+          <ComboBox
+            key='projection'
+            id='projection'
+            state={this.props.state.projection}
+            onUpdate={this.props.changeHandler}
+            data={[
+              { id: "EPSG:3857", value: _("Global") },
+              { id: "EPSG:32661", value: _("Arctic") },
+              { id: "EPSG:3031", value: _("Antarctic") },
+            ]}
+            title={_("Projection")}
+          />
+          <ComboBox
+            key='basemap'
+            id='basemap'
+            state={this.props.state.basemap}
+            onUpdate={this.props.changeHandler}
+            data={[
+              {
+                id: "topo",
+                value: _("ETOPO1 Topography"),
+                attribution: "Topographical Data from ETOPO1 1 Arc-Minute Global Relief Model. NCEI, NESDIR, NOAA, U.S. Department of Commerce"
+              },
+              {
+                id: "ocean",
+                value: _("Esri Ocean Basemap"),
+                attribution: "Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri"
+              },
+              {
+                id: "world",
+                value: _("Esri World Imagery"),
+                attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+              },
+            ]}
+            title={_("Basemap")}
+          />
+          <SelectBox
+            key='bathymetry'
+            id='bathymetry'
+            state={this.props.state.bathymetry}
+            onUpdate={this.props.changeHandler}
+            title={_("Show Bathymetry Contours")}
+          />
+          <SelectBox
+            key='dataset_compare'
+            id='dataset_compare'
+            state={this.props.state.dataset_compare}
+            onUpdate={this.props.changeHandler}
+            title={_("Compare Datasets")}
+          />
+        </Panel>
+        <Panel
+          header={this.props.state.dataset_compare ? "Left View" : "Primary View"}
+          expanded
+          bsStyle='primary'
+          >
           <DatasetSelector
-            key='dataset_1'
-            id='dataset_1'
-            state={this.props.state.dataset_1}
+            key='dataset_0'
+            id='dataset_0'
+            state={this.props.state}
             onUpdate={this.props.changeHandler}
             depth={true}
           />
+        </Panel>
+        <div style={{"display": this.props.state.dataset_compare ? "block" : "none"}}>
+          <Panel
+            header="Right View"
+            expanded
+            bsStyle='primary'
+          >
+            <DatasetSelector
+              key='dataset_1'
+              id='dataset_1'
+              state={this.props.state.dataset_1}
+              onUpdate={this.props.changeHandler}
+              depth={true}
+            />
+          </Panel>
         </div>
         <Range
           key='scale'
