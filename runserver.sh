41d51238038aaabfc90dbb4e2c12b55910c25eca
Nabil M <nmiri127@gmail.com>
2018-07-03 09:48:14 -0230

    Added usage comment to runserver.sh
    * Since $1 doesn't tell you what it wants lol.

--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# Usage: ./runserver.sh <optional_dataset_config_file.json>
+# <optional_dataset_config_file.json>:  Specify a non-default dataset config file to load the Navigator with.
+#                                       Argument not required.
+
 gunicorn -w 12 -t 90 --graceful-timeout 90 --preload -b 0.0.0.0:5000 --reload "oceannavigator:create_app()" $1
