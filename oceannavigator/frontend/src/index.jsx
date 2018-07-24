f6109cef4ad213a8159ee6056f9bd5422bf4075d
Noah Gallant <noah.gallant@mun.ca>
2018-07-24 10:33:20 -0230

    Fixed react-ga build error

--- oceannavigator/frontend/src/index.jsx
+++ oceannavigator/frontend/src/index.jsx
@@ -3,14 +3,13 @@ import {render} from "react-dom";
 import OceanNavigator from "./components/OceanNavigator.jsx";
 import WebFont from "webfontloader";
 import Browser from "detect-browser";
+import ReactGA from 'react-ga';
 
 const i18n = require("./i18n.js");
 
 require("bootstrap/dist/css/bootstrap.css");
 require("./stylesheets/utils/bootstrap.css");
 require("./stylesheets/main.scss");
-
-import ReactGA from 'react-ga'
 
 ReactGA.initialize('UA-122671965-2')
 ReactGA.pageview(window.location.pathname + window.location.search)
@@ -19,7 +18,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <OceanNavigator />
+          <OceanNavigator/>
       </div>
     );
   }
