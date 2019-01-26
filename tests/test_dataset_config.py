164476000bf0f16ebca469b81812c2c389ecd26c
Geoff Holden <geoff@geoffholden.com>
2019-01-26 10:40:13 -0330

    Fixing another unit test.
    Looks like this test was refactored but then never actually run.

--- tests/test_dataset_config.py
+++ tests/test_dataset_config.py
@@ -1,8 +1,12 @@
 import unittest
-from unittest.mock import patch
-from oceannavigator import dataset_config
+from unittest.mock import patch, Mock
+from oceannavigator import dataset_config, create_app
 
 class TestUtil(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestUtil, self).__init__(*args, **kwargs)
+        self.app = create_app()
 
     """
     @patch("ConfigParser.RawConfigParser")
@@ -19,17 +23,25 @@ class TestUtil(unittest.TestCase):
 
     @patch("oceannavigator.dataset_config.__get_dataset_config")
     def test_get_datasets(self, m):
-        m.return_value.items.return_value = {
-            "k": "\"v\"",
-            "key": "\"value\"",
-        }.items()
+        m.return_value = {
+            "k": {
+                "enabled": True,
+            },
+            "key": {
+                "enabled": True,
+            },
+            "disabled": {
+                "notenabled": True,
+            },
+        }
 
         result = dataset_config.get_datasets()
         self.assertEqual(len(result), 2)
-        self.assertEqual(result['k'], 'v')
-        self.assertEqual(result['key'], 'value')
+        self.assertIn('k', result)
+        self.assertIn('key', result)
+        self.assertNotIn('disabled', result)
 
-    @patch("oceannavigator.dataset_config.get_datasets")
+    @patch("oceannavigator.dataset_config.__get_dataset_config")
     def test_get_dataset_misc(self, m):
         m.return_value = {
             "dataset": {
@@ -55,84 +67,101 @@ class TestUtil(unittest.TestCase):
 
     @patch("oceannavigator.dataset_config.__get_dataset_config")
     def test_get_variables(self, m):
-        m.return_value.items.return_value = {
-            "k": "\"v\"",
-            "key": "\"value\"",
-        }.items()
+        m.return_value = {
+            "ds": {
+                "variables": {
+                    "k": {
+                    },
+                    "key": {
+                    }
+                }
+            }
+        }
 
         result = dataset_config.get_variables("ds")
-        m.return_value.items.assert_called_with("ds")
         self.assertEqual(len(result), 2)
-        self.assertEqual(result['k'], 'v')
-        self.assertEqual(result['key'], 'value')
+        self.assertIn('k', result)
+        self.assertIn('key', result)
 
-    @patch("oceannavigator.dataset_config.get_variables")
+    @patch("oceannavigator.dataset_config.__get_dataset_config")
     def test_get_variable_misc(self, m):
         m.return_value = {
-            "var": {
-                "name": "the_name",
-                "unit": "My Unit",
-                "scale": [0, 10],
-            },
-            "var2": {
-                "hide": True,
+            "dataset": {
+                "url": "the_url",
+                "attribution": "My attribution <b>bold</b>",
+                "climatology": "climatology_url",
+                "cache": 5,
+                "variables": {
+                    "var": {
+                        "name": "the_name",
+                        "unit": "My Unit",
+                        "scale": [0, 10],
+                    },
+                    "var2": {
+                        "hide": True,
+                    }
+                }
             }
         }
 
         self.assertEqual(
-            util.get_variable_name("dataset", mock.Mock(key="var")),
+            dataset_config.get_variable_name("dataset", Mock(key="var")),
             "the_name"
         )
-        variable_mock = mock.Mock()
+        variable_mock = Mock()
         variable_mock.configure_mock(key="none", name="var_name")
         self.assertEqual(
-            util.get_variable_name("dataset", variable_mock),
+            dataset_config.get_variable_name("dataset", variable_mock),
             "var_name"
         )
         variable_mock.configure_mock(key="nameless", name=None)
         self.assertEqual(
-            util.get_variable_name("dataset", variable_mock),
+            dataset_config.get_variable_name("dataset", variable_mock),
             "Nameless"
         )
 
         self.assertEqual(
-            util.get_variable_unit("dataset", mock.Mock(key="var")),
+            dataset_config.get_variable_unit("dataset", Mock(key="var")),
             "My Unit"
         )
         variable_mock.configure_mock(key="none", unit="var_unit")
         self.assertEqual(
-            util.get_variable_unit("dataset", variable_mock),
+            dataset_config.get_variable_unit("dataset", variable_mock),
             "var_unit"
         )
         self.assertEqual(
-            util.get_variable_unit(
+            dataset_config.get_variable_unit(
                 "dataset", Mock(key="varx", unit=None)),
             "Unknown"
         )
 
         self.assertEqual(
-            util.get_variable_scale("dataset", Mock(key="var")),
+            dataset_config.get_variable_scale("dataset", Mock(key="var")),
             [0, 10]
         )
         variable_mock.configure_mock(key="none", valid_min=5, valid_max=50)
         self.assertEqual(
-            util.get_variable_scale(
+            dataset_config.get_variable_scale(
                 "dataset", variable_mock),
             [5, 50]
         )
         self.assertEqual(
-            util.get_variable_scale(
+            dataset_config.get_variable_scale(
                 "dataset", Mock(key="varx", scale=None, valid_min=None,
                                      valid_max=None)),
             [0, 100]
         )
 
         self.assertFalse(
-            util.is_variable_hidden("dataset", Mock(key="var"))
+            dataset_config.is_variable_hidden("dataset", Mock(key="var"))
         )
         self.assertTrue(
-            util.is_variable_hidden("dataset", Mock(key="var2"))
+            dataset_config.is_variable_hidden("dataset", Mock(key="var2"))
         )
-        self.assertFalse(
-            util.is_variable_hidden("dataset", Mock(key="var3"))
-        )
+
+    def setUp(self):
+        self.ctx = self.app.app_context()
+        self.ctx.push()
+
+    def tearDown(self):
+        self.ctx.pop()
