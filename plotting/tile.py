e8cbbd38558d64a9a03918b65a146f96b7e8c1fb
haleh <halehtaghdisi@gmail.com>
2019-02-21 13:12:45 -0330

    a

--- plotting/tile.py
+++ plotting/tile.py
@@ -526,13 +526,14 @@ def bathymetry(projection, x, y, z, args):
 
     xpx = x * 256
     ypx = y * 256
-   try:
+    try:
        with Dataset(current_app.config['ETOPO_FILE'] % (projection, z), 'r') as dataset:
         data = dataset["z"][ypx:(ypx + 256), xpx:(xpx + 256)] * -1
         data = data[::-1, :]
 
        pass
-    except: pass 
+    except:
+          pass 
     LEVELS = [100, 200, 500, 1000, 2000, 3000, 4000, 5000, 6000]
 
     normalized = matplotlib.colors.LogNorm(vmin=1, vmax=6000)(LEVELS)
