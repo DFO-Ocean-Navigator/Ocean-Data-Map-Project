46bb143012922da33c5be108318d8cbfb71c6739
Nabil M <nmiri127@gmail.com>
2019-05-07 14:57:24 -0230

    Update .eslintrc
    * Add strict rules to linter.

--- oceannavigator/frontend/.eslintrc.js
+++ oceannavigator/frontend/.eslintrc.js
@@ -1,73 +1,115 @@
 module.exports = {
-   
-  "env": {
-    "browser": true,
-    "es6": true,
-    "node" : true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true
   },
-  "extends": ["eslint:recommended", "plugin:react/recommended"],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true,
-      "templateStrings": true
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
+      templateStrings: true
     },
-    "sourceType": "module"
+    sourceType: "module"
   },
-  "plugins": [
-    "react"
-  ],
-  "rules": {
-    "indent": [
+  plugins: ["react"],
+  rules: {
+    indent: [
       "error",
       2,
       {
-        "SwitchCase": 1
+        SwitchCase: 1
       }
     ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "double"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ],
-    "curly": [
-      "error",
-      "all"
-    ],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    curly: ["error", "all"],
     "no-console": [
       "error",
       {
-        "allow": ["warn", "error"]
+        allow: ["warn", "error"]
       }
     ],
     "no-unused-vars": [
       "error",
       {
-        "varsIgnorePattern": "i18n",
-        "args": "none"
+        varsIgnorePattern: "i18n",
+        args: "none"
       }
     ],
     "max-len": [
       "error",
       {
-        "code": 80,
-        "ignoreStrings": true
+        code: 80,
+        ignoreStrings: true
       }
     ],
     "react/jsx-uses-vars": "error",
     "react/jsx-uses-react": "error",
+    // Node-specific problems
+    "callback-return": "error",
+    "handle-callback-err": "error",
+    "no-buffer-constructor": "error",
+    "no-new-require": "error",
+    "no-path-concat": "error",
+    // ES2015+ specific problems
+    "no-duplicate-imports": "error",
+    "no-useless-computed-key": "error",
+    "no-useless-constructor": "error",
+    "no-useless-rename": "error",
+    // Force esnext features
+    "no-var": "error",
+    "object-shorthand": "error",
+    "prefer-arrow-callback": "error",
+    "prefer-numeric-literals": "error",
+    "prefer-template": "error",
+    "prefer-spread": "error",
+    "prefer-rest-params": "error",
+    // NOTE: Very opinionated:
+    "prefer-const": "error",
+    // Additional problems
+    "array-callback-return": "error",
+    "dot-notation": "error",
+    eqeqeq: ["error", "smart"],
+    "no-caller": "error",
+    "no-else-return": "error",
+    "no-eval": "error",
+    "no-extend-native": "error",
+    "no-extra-bind": "error",
+    "no-implied-eval": "error",
+    "no-iterator": "error",
+    "no-labels": "error",
+    "no-lone-blocks": "error",
+    "no-multi-str": "error",
+    "no-new-func": "error",
+    "no-new-wrappers": "error",
+    "no-octal-escape": "error",
+    "no-proto": "error",
+    "no-return-await": "error",
+    "no-script-url": "error",
+    "no-self-compare": "error",
+    "no-sequences": "error",
+    "no-unmodified-loop-condition": "error",
+    "no-useless-call": "error",
+    "no-useless-return": "error",
+    "no-with": "error",
+    yoda: [
+      "error",
+      "never",
+      {
+        onlyEquality: true
+      }
+    ],
+    strict: "error",
+    "no-undef-init": "error",
+    "no-use-before-define": ["error", "nofunc"]
   },
-  "globals": {
-    "$": true,
-    "jQuery": true,
-    "_": true,
-    "require": true,
+  globals: {
+    $: true,
+    jQuery: true,
+    _: true,
+    require: true
   }
 };
