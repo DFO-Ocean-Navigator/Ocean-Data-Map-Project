d7644f024790f79fa548cf88399690308a8032fb
Nabil M <nmiri127@gmail.com>
2019-08-12 15:44:27 -0230

    Migrate depths API endpoint to 1.0

--- data/__init__.py
+++ data/__init__.py
@@ -92,6 +92,9 @@ def __get_nc_file_list(url: str, **kwargs):
         return db.get_netcdf_files(
             timestamp, variable)
 
+    if not isinstance(kwargs['timestamp'], list):
+        kwargs['timestamp'] = list(kwargs['timestamp'])
+
 
 def __is_mercator(variable_list: list):
     return 'latitude_longitude' in variable_list or \
