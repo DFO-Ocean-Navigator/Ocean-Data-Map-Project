ab14b3084b9b3d8ae25b7a399012effdba1d94a4
Nabil Miri <nmiri127@gmail.com>
2019-08-24 14:19:40 -0230

    Increase resolution of plot minimap from crude to low

--- plotting/utils.py
+++ plotting/utils.py
@@ -100,7 +100,7 @@ def _map_plot(points, path=True, quiver=True):
         urcrnrlat=maxlat,
         lat_0=np.mean(points[0, :]),
         lon_0=np.mean(points[1, :]),
-        resolution='c', projection='merc',
+        resolution='l', projection='merc',
         rsphere=(6378137.00, 6356752.3142),
     )    
 
