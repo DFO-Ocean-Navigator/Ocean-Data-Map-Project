3a1156c6e0f36496eaf41ab09a4a90eb15f5ebe5
James Rayner <36578069+jamesprayner@users.noreply.github.com>
2019-09-03 14:43:27 -0230

    Rewrite python script generation + unit tests (#476)

--- plotting/scriptGenerator.py
+++ plotting/scriptGenerator.py
@@ -5,16 +5,8 @@ import base64
 import pytz
 import hashlib
 import routes.routes_impl
-
-import json
-from io import BytesIO, StringIO
-import re
-import base64
-import pytz
-import hashlib
 from data import open_dataset
 from oceannavigator import DatasetConfig
-
 
 def time_query_conversion(dataset, index):
     """
@@ -33,110 +25,40 @@ def time_query_conversion(dataset, index):
             return date.replace(tzinfo=pytz.UTC).isoformat()
         except IndexError:
             return ClientError("Timestamp does not exist")
-             
-def generatePython(url):
 
-    isClass4 = False
-    notPlot = False
-    netcdf = False
-    data_type = "plot"
+def generatePython(url, scriptType):
     if "class4id" in url:
-        isClass4 = True
-    try:
-        url = json.loads(url)
-    except:
-        notPlot = True
-        url_tail = "&" + re.findall("&(.*)", url)[0]
-        url = re.sub("&(.*)", "", url)
-        url = json.loads(url)
-        fileExtension = "csv"
-    if 'output_format' in url:
-        netcdf = True
-        fileExtension = "nc"
-        data_type = "subset"
-    
-    #setup file
-    script = StringIO()
-    #FILE CONTENTS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    #HEADER---------------------
-    if notPlot == True or netcdf == True:
-        script.write("import requests\n")
-        script.write("import os\n")
-        script.write("import shutil\n")
-    script.write("from urllib.request import urlopen\n")
-    script.write("from urllib.parse import urlencode\n")
-    script.write("from contextlib import closing\n")
-    script.write("try:\n")
-    script.write("   from PIL import Image\n")
-    script.write("except:\n")   
-    script.write('   print("If you are on a Windows machine, please install PIL (imaging library) using' + " 'python -m pip install Pillow'" + ' in the Anaconda Prompt")\n')
-    script.write("   exit()\n")
-    script.write("import json\n")
-    script.write("\n\n")
-    #Set Navigators URL
-    script.write("#Set Navigator URL\n")
-    if not isClass4:
-        script.write('base_url = "http://navigator.oceansdata.ca/api/v1.0/%s/?"\n\n' % (data_type))
+        var = "class4id"
+    elif "drifter" in url:
+        var = "drifter"
     else:
-        script.write('base_url = "http://navigator.oceansdata.ca/%s/?"\n\n' % (data_type))
-    #---------------------------
-    
-        
-    #CREATE JSON OBJECT---------
-    script.write("#Create JSON Object\n")
-    script.write("query = {\n")
-    for x in url:
-      #print(type(url.get(x)))
-      if isinstance(url.get(x), str):
-        script.write('  "' + x + '": "' + str(url.get(x)) + '"' + ",\n" )
-      else:
-        
-        if x == 'time':
-            formatted_date = time_query_conversion(url.get('dataset'),url.get('time'))
-            script.write('  "' + x + '": ' + "'" + formatted_date + "',\n")
-        else:
-            script.write('  "' + x + '": ' + str(url.get(x)) + ",\n")
-    script.write("}\n")
-    #---------------------------
-    #Assemble full request
-    script.write('\n#Assemble full request - converts json object to url\n')
-    if notPlot == False:
-        script.write("url = base_url + urlencode(" + '{"query": ' + "json.dumps(query)})" + "\n")
-    else:
-        script.write("url = base_url + urlencode(" + '{"query": ' + "json.dumps(query)}) + '" + url_tail + "'\n")
-    
-    script.write("print(url)\n")
-    #Open URL and read response
-    if notPlot == False and netcdf == False:
-        script.write("\n#Open URL and save response\n")
-        script.write("with closing(urlopen(url)) as f:\n")
-        script.write("  img = Image.open(f)\n")
-        if url.get("type") == "drifter":
-            script.write('  img.save("script_template_" + str(query["dataset"]) + "_" + str(query["drifter"]) + ".png", "PNG")\n')
-        elif isClass4:
-            script.write('  img.save("script_template_" + str(query["dataset"]) + "_" + str(query["class4id"]) + ".png" , "PNG")\n')
-        else:
-            script.write('  img.save("script_template_" + str(query["dataset"]) + "_" + str(query["variable"]) + ".png" , "PNG")\n')
-    else:
-        script.write("\n#Open URL and save response\n")
-        script.write("data_file = requests.get(url, stream=True)\n")
-        script.write("dump = data_file.raw\n")
-        script.write("location = os.path.abspath('/home/script_output.%s')\n" % (fileExtension))
-        script.write('with open("script_output.%s", "wb") as location:\n' % (fileExtension))
-        script.write("  print('Saving File')\n")
-        script.write("  shutil.copyfileobj(dump, location)\n")
-        script.write("  print('Done')\n")
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    
-    #CONVERT TO BytesIO TO SEND
-    b = BytesIO()
-    script.seek(0)
-    b.write(bytes(script.read(),'utf-8'))
-    b.seek(0)
-    
-    return b
+        var = "variable"
+    query = url
 
-def generateR(url):
+    # format query for python, could do with regular expressions
+    query = query.replace(',"', ',\n"')
+    if "true" in query:
+        query = query.replace("true", "1")
+    if "false" in query:
+        query = query.replace("false", "0")
+    if "null"  in query:
+        query = query.replace("null", "None")
+    
+    with open("plotting/templates/python" + scriptType + "template.txt", 'r') as f:
+        template = str(f.read())
+
+        if scriptType == "PLOT":
+            template = template.format(q = query, var = var)
+        else:
+            template = template.format(q = query)
+
+        finalScript = BytesIO()
+        finalScript.write(bytes(template,'utf-8'))
+        finalScript.seek(0)
+        return finalScript
+
+
+def generateR(url, scriptType):
 
     isClass4 = False
     notPlot = False
@@ -241,7 +163,3 @@ def generateR(url):
     #b.seek(0)
 
     return b
-
-
-
-    
--- routes/api_v1_0.py
+++ routes/api_v1_0.py
@@ -43,15 +43,15 @@ bp_v1_0 = Blueprint('api_v1_0', __name__)
 # API INTERFACE 
 #~~~~~~~~~~~~~~~~~~~~~~~
 
-@bp_v1_0.route("/api/v1.0/generatescript/<string:url>/<string:type>/")
-def generateScript(url: str, type: str):
+@bp_v1_0.route("/api/v1.0/generatescript/<string:query>/<string:lang>/<string:scriptType>/")
+def generateScript(query: str, lang: str, scriptType: str):
 
-  if type == "python":
-    b = generatePython(url)
+  if lang == "python":
+    b = generatePython(query, scriptType)
     resp = send_file(b, as_attachment=True, attachment_filename='script_template.py', mimetype='application/x-python')
     
-  elif type == "r":
-    b = generateR(url)
+  elif lang == "r":
+    b = generateR(query, scriptType)
     resp = send_file(b, as_attachment=True, attachment_filename='script_template.r', mimetype='application/x-python')
   
   return resp
