ff9c2d09abaea4390e862e8b2c8c9f15d9bc8c51
Noah Gallant <noah.gallant@mun.ca>
2019-09-30 10:26:25 -0300

    Changed timestamp to time

--- routes/api_v1_0.py
+++ routes/api_v1_0.py
@@ -462,7 +462,7 @@ def timestamp_for_date_v1_0(old_dataset: str, date: int, new_dataset: str):
 @bp_v1_0.route('/api/v1.0/tiles/<string:interp>/<int:radius>/<int:neighbours>/<string:projection>/<string:dataset>/<string:variable>/<string:time>/<string:depth>/<string:scale>/<int:masked>/<string:display>/<int:zoom>/<int:x>/<int:y>.png')
 def tile_v1_0(projection: str, interp: str, radius: int, neighbours: int, dataset: str, variable: str, time: str, depth: str, scale: str, masked: int, display: str, zoom: int, x: int, y: int):
 
-    return routes.routes_impl.tile_impl(projection, interp, radius, neighbours, dataset, variable, timestamp, depth, scale, masked, display, zoom, x, y)
+    return routes.routes_impl.tile_impl(projection, interp, radius, neighbours, dataset, variable, time, depth, scale, masked, display, zoom, x, y)
 
 
 @bp_v1_0.route('/api/v1.0/tiles/topo/<string:shaded_relief>/<string:projection>/<int:zoom>/<int:x>/<int:y>.png')
