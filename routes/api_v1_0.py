5e5a36f183719e0763cba4a55c4e96902dc068cb
Noah Gallant <noah.gallant@mun.ca>
2019-09-30 10:24:44 -0300

    tile update

--- routes/api_v1_0.py
+++ routes/api_v1_0.py
@@ -462,10 +462,6 @@ def timestamp_for_date_v1_0(old_dataset: str, date: int, new_dataset: str):
 @bp_v1_0.route('/api/v1.0/tiles/<string:interp>/<int:radius>/<int:neighbours>/<string:projection>/<string:dataset>/<string:variable>/<string:time>/<string:depth>/<string:scale>/<int:masked>/<string:display>/<int:zoom>/<int:x>/<int:y>.png')
 def tile_v1_0(projection: str, interp: str, radius: int, neighbours: int, dataset: str, variable: str, time: str, depth: str, scale: str, masked: int, display: str, zoom: int, x: int, y: int):
 
-    config = DatasetConfig(dataset)
-    timestamp = datetime_to_timestamp(
-        string_to_datetime(time), config.time_dim_units)
-
     return routes.routes_impl.tile_impl(projection, interp, radius, neighbours, dataset, variable, timestamp, depth, scale, masked, display, zoom, x, y)
 
 
