7add61c5fea1bbd822f1e9b1ba47b3324f80a3e5
Noah Gallant <noah.gallant@mun.ca>
2019-10-04 11:28:06 -0300

    readded min and max

--- plotting/sound.py
+++ plotting/sound.py
@@ -75,7 +75,7 @@ class SoundSpeedPlotter(TemperatureSalinityPlotter):
         minpos = self.temperature_depths.data[0][minpos][0]
         maxpos = np.where(self.sspeed[0] == maxspeed)
 
-        plt.axvline(x=minspeed)
+        plt.axvline(x=minspeed, ymin=0, ymax=2000)
 
         ax.set_xlim([
             np.amin(self.sspeed) - (maxspeed - minspeed) * 0.1,
