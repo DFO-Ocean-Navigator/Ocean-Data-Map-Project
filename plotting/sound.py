9f910b2d2bdcbd0bdede2c71fab8a43ac0577641
Noah Gallant <noah.gallant@mun.ca>
2019-10-04 16:24:41 -0230

    Adjusted display value

--- plotting/sound.py
+++ plotting/sound.py
@@ -118,7 +118,7 @@ class SoundSpeedPlotter(TemperatureSalinityPlotter):
 
         depthexcess = np.amax(self.sspeed) + (maxspeed - minspeed) - criticaldepth_true
 
-        plt.text( soniclayerdepth_value - 10, criticaldepth_true - (depthexcess / 2), str(depthexcess))
+        plt.text( soniclayerdepth_value - 10, (criticaldepth_true + (depthexcess / 2)), str(depthexcess))
         # ~~~~~~~~~~~~
         
 
