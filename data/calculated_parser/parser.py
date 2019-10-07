bd51c249321c7a925dc71d7b01804b287d5c6e42
Noah Gallant <noah.gallant@mun.ca>
2019-10-07 14:14:41 -0300

    removed 1 breakpoint

--- data/calculated_parser/parser.py
+++ data/calculated_parser/parser.py
@@ -69,7 +69,6 @@ class Parser:
                 key = [d[k] for k in variable.dimensions]
         except KeyError:
             raise SyntaxError
-        print(something)
         return tuple(key)
 
     # Similar to the Lexer, these p_*, methods cannot have proper python
