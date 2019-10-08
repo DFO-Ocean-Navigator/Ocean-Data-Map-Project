cecba5f24f798c689ff75ead87ed84e9b3310c1c
Noah Gallant <noah.gallant@mun.ca>
2019-10-08 11:10:32 -0300

    test

--- data/calculated_parser/parser.py
+++ data/calculated_parser/parser.py
@@ -76,11 +76,18 @@ class Parser:
 
     def p_expression_variable(self, t):
         'expression : ID'
+        print(something)
         t[0] = self.data.variables[t[1]][
                 self.get_key_for_variable(
                     self.data.variables[t[1]]
                     )
                 ]
+
+    def p_expression_variable_all(self, t):
+        'expression : TILDA ID'
+
+        print(something)
+        t[0] = self.data.variables[t[1]]
 
     def p_expression_uop(self, t):
         '''expression : MINUS expression %prec UMINUS'''
