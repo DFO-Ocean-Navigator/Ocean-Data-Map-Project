dbd8ce1a20711ce14d6b9bc974862b03876ef1b7
Noah Gallant <noah.gallant@mun.ca>
2019-10-11 10:42:27 -0300

    initializing cd_depth

--- data/calculated_parser/functions.py
+++ data/calculated_parser/functions.py
@@ -255,6 +255,7 @@ def criticaldepth(depth, lat, lon, temperature, salinity):
     sca = 0
     for x in range(speed.shape[0]):
         for y in range(speed.shape[1]):
+            cd_depth = np.nan
             if (speed[x][y].size - np.count_nonzero(np.isnan(speed[x][y]))) != 0:
                 speed_point = speed[x][y]
                 sca_idx = find_sca_idx(speed_point)
