fc84d38eed91e3f8446997a158ef974aa99e3de3
Noah Gallant <noah.gallant@mun.ca>
2019-10-11 13:38:19 -0300

    test

--- plotting/sound.py
+++ plotting/sound.py
@@ -128,6 +128,7 @@ class SoundSpeedPlotter(TemperatureSalinityPlotter):
                 max_depth = self.temperature_depths.data[0][ss.count() - 1] #- criticaldepth_true
                 depthexcess = max_depth - criticaldepth_true
                 depthexcess = float("{0:.2f}".format(depthexcess))
+                plt.text( sonilayerdepth_value - 10, (criticaldepth_true + (depthexcess / 2) + 5), "Depth Excess")
                 plt.text( soniclayerdepth_value - 10, (criticaldepth_true + (depthexcess / 2)), str(depthexcess) + 'm')
                 # ~~~~~~~~~~~~
 
