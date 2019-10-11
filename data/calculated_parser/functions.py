00fa6d23438a650857935cd67206ba80ffc69f98
Noah Gallant <noah.gallant@mun.ca>
2019-10-11 11:14:02 -0300

    Checking for (-) depth excess

--- data/calculated_parser/functions.py
+++ data/calculated_parser/functions.py
@@ -319,7 +319,8 @@ def depthexcess(depth, lat, lon, temperature, salinity):
                             total_idx = speed_point.size - np.count_nonzero(np.isnan(speed_point)) - 1
                             max_depth = depth.values[total_idx]
                             depth_excess = max_depth - cd_depth
-                            
+                            if depth_excess < 0:
+                                depth_excess = np.nan
                         else:
                             depth_excess = np.nan
                     else:
