4f944bad7291ba5b87ae6880940ee6a1a92d00bf
Noah Gallant <noah.gallant@mun.ca>
2019-10-11 14:56:20 -0300

    Improved speed

--- data/calculated_parser/functions.py
+++ data/calculated_parser/functions.py
@@ -88,16 +88,16 @@ def sspeedmin(depth, lat, lon, temperature, salinity):
     
     for x in range(speed.shape[0]):
         for y in range(speed.shape[1]):
-            min_val = np.nanmin(speed[x][y])
-            idx = np.where(speed[x][y] == min_val)
+            min_val = np.nanmin(speed[x,y])
+            idx = np.where(speed[x,y] == min_val)
             if (np.isnan(min_val)):
-                speed[x][y] = np.nan
+                speed[x,y] = np.nan
             elif idx[0].shape[0] > 1:
                 idx = idx[0][0]
 
-                speed[x][y] = depth.values[idx]
+                speed[x,y] = depth.values[idx]
             else:
-                speed[x][y] = depth.values[idx]
+                speed[x,y] = depth.values[idx]
             #speed[x][y] = depth[np.where(speed[x][y] == np.nanmin(speed[x][y]))]  #np.nanmin(speed[x][y])
     
     speed = speed.transpose()
