ba600c9c46797de059a9cb43af64235c15b51d29
Noah Gallant <noah.gallant@mun.ca>
2019-10-11 15:02:12 -0300

    testing new iterating method

--- data/calculated_parser/functions.py
+++ data/calculated_parser/functions.py
@@ -86,19 +86,20 @@ def sspeedmin(depth, lat, lon, temperature, salinity):
     speed = sspeed(depth, lat, temperature, salinity)
     speed = speed.transpose()
     
-    for x in range(speed.shape[0]):
-        for y in range(speed.shape[1]):
-            min_val = np.nanmin(speed[x,y])
-            idx = np.where(speed[x,y] == min_val)
-            if (np.isnan(min_val)):
-                speed[x,y] = np.nan
-            elif idx[0].shape[0] > 1:
-                idx = idx[0][0]
-
-                speed[x,y] = depth.values[idx]
-            else:
-                speed[x,y] = depth.values[idx]
-            #speed[x][y] = depth[np.where(speed[x][y] == np.nanmin(speed[x][y]))]  #np.nanmin(speed[x][y])
+    #for x in range(speed.shape[0]):
+    #    for y in range(speed.shape[1]):
+    for xy in np.ndindex(y.shape[:2])
+        print(something)
+        min_val = np.nanmin(speed[x,y])
+        idx = np.where(speed[x,y] == min_val)
+        if (np.isnan(min_val)):
+            speed[x,y] = np.nan
+        elif idx[0].shape[0] > 1:
+            idx = idx[0][0]
+            speed[x,y] = depth.values[idx]
+        else:
+            speed[x,y] = depth.values[idx]
+        #speed[x][y] = depth[np.where(speed[x][y] == np.nanmin(speed[x][y]))]  #np.nanmin(speed[x][y])
     
     speed = speed.transpose()
 
