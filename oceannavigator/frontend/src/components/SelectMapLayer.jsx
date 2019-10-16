54bedf00a0a4add77bcf0998c8f022f15c33249f
Noah Gallant <noah.gallant@mun.ca>
2019-10-16 13:10:49 -0300

    test

--- oceannavigator/frontend/src/components/SelectMapLayer.jsx
+++ oceannavigator/frontend/src/components/SelectMapLayer.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import * as ol from "ol";
+import * as olproj from "ol/proj";
+import * as olproj4 from "ol/proj/proj4";
+import * as olcontrol from "ol/control";
+import * as olsource from "ol/source";
+import * as olloadingstrategy from "ol/loadingstrategy";
+import * as olformat from "ol/format";
+import * as oltilegrid from "ol/tilegrid";
+import * as ollayer from "ol/layer";
+import * as olstyle from "ol/style";
+import * as olinteraction from "ol/interaction";
+import * as olcondition from "ol/events/condition";
+import * as olgeom from "ol/geom";
+import * as olextent from "ol/extent";
 
 export default class SelectMapLayer extends React.Component {
     constructor (props) {
