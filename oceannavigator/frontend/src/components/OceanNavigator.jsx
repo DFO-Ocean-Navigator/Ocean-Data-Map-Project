cacf56381d06c456db7f8c53824dafb17933e08f
Noah Gallant <noah.gallant@mun.ca>
2019-10-16 12:03:04 -0300

    data as map layers

--- oceannavigator/frontend/src/components/OceanNavigator.jsx
+++ oceannavigator/frontend/src/components/OceanNavigator.jsx
@@ -763,7 +763,8 @@ export default class OceanNavigator extends React.Component {
           <Modal.Body>
             <ModalContainer
               modal={this.state.modal}
-              data={this.state.data}
+              //data={this.state.data}
+              data={this.mapComponent.getLayers()}
               area={this.state.area}
               names={this.state.names}
               point={this.state.point}
