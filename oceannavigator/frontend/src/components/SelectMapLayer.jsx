5dbcbec6001bed7f22d2bc3d2e1e36881849fa3a
Noah Gallant <noah.gallant@mun.ca>
2019-10-16 13:01:27 -0300

    Looping through layers

--- oceannavigator/frontend/src/components/SelectMapLayer.jsx
+++ oceannavigator/frontend/src/components/SelectMapLayer.jsx
@@ -10,7 +10,10 @@ export default class SelectMapLayer extends React.Component {
 
         let buttons = undefined;
 
-        for (let x in this.props.map) {
+        console.warn("MAP: ", this.props.map)
+        let layers = this.props.map.getLayers();
+        console.warn("LAYERS: ", layers)
+        for (let x in layers) {
             console.warn("X: ", x);
         }
 
