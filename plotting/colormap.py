688ff494866b32fe8f64360321fa01667cb43cb7
Noah Gallant <noah.gallant@mun.ca>
2019-10-22 13:39:08 -0300

    Colourmaps

--- plotting/colormap.py
+++ plotting/colormap.py
@@ -188,6 +188,7 @@ colormap_names = {
     'iron': 'Iron',
     'mercator_current': 'Mercator Ocean Current',
     'mercator': 'Mercator',
+    'matter': 'Matter'
     'nitrate': 'Nitrate',
     'oxygen': 'Oxygen',
     'phosphate': 'Phosphate',
