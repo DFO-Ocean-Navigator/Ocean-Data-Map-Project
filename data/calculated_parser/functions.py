81140b2a714f08b60c87be54f09df33ce768a1f2
Noah Gallant <noah.gallant@mun.ca>
2019-10-25 12:01:51 -0300

    Check for nan

--- data/calculated_parser/functions.py
+++ data/calculated_parser/functions.py
@@ -304,11 +304,12 @@ def slopeofsomething_point(sspeed, depth):
 
         temp_sspeed = temp_sspeed[:temp_sspeed.shape[0]-1]
         temp_depth = temp_sspeed[:temp_depth.shape[0]-1]
-
+        if np.isnan(np.min(temp_sspeed)) or np.isnan(np.min(temp_depth)):
+            print(something)
         new_slope, intercept, r_value, p_value, std_err = linregress(temp_sspeed, temp_depth)
 
         # Determine breaking condition
-        if previous_slope - new_slope < 0:
+        if previous_slope - new_slope < 0.5:
             return previous_slope
         elif temp_sspeed.shape[0] == 2:
             return np.nan
