dbe9a445214c9a4c85e2dd155d18561c358079df
Noah Gallant <noah.gallant@mun.ca>
2019-10-28 15:24:19 -0300

    fixed np.array creation

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -9,7 +9,7 @@ class TestFunctions(unittest.TestCase):
         
     
     def test_sscp_point(self):
-        test_array = np.array[1434.08,1435.48,1436.10,1435.16,1433.01,1432.80,1432.54,1432.75,1433.20,1434.92,1435.81]
+        test_array = np.array([1434.08,1435.48,1436.10,1435.16,1433.01,1432.80,1432.54,1432.75,1433.20,1434.92,1435.81])
         test_max = 11
 
         test_result = sscp_point(test_array, test_max)
