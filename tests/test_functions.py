ed99feac952fa953e4a41f9cab2292890492e4e8
Noah Gallant <noah.gallant@mun.ca>
2019-10-28 15:42:44 -0300

    changed function arg order

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -30,7 +30,7 @@ class TestFunctions(unittest.TestCase):
         test_array = np.array([1434.08, 1435.48, 1436.10, 1435.16, 1433.01, 1432.80, 1432.54, 1432.75, 1433.20, 1434.92, 1435.81, 1436.80, 1437.91])
         sca_idx = 6
 
-        test_result = find_sld_idx(test_array, sca_idx)
+        test_result = find_sld_idx(sca_idx, test_array)
         self.assertEqual(test_result, 2)
 
     #def test_soundchannelaxis(self):
