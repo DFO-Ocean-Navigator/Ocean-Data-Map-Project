c72805b41c9abd004e4133107450697b01ade66d
Noah Gallant <noah.gallant@mun.ca>
2019-10-28 15:50:29 -0300

    testing cd_idx

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -33,6 +33,15 @@ class TestFunctions(unittest.TestCase):
         test_result = find_sld_idx(sca_idx, test_array)
         self.assertEqual(test_result, 2)
 
+
+    def test_find_cd_idx(self):
+        test_array = np.array([1434.08, 1435.48, 1436.10, 1435.16, 1433.01, 1432.80, 1432.54, 1432.75, 1433.20, 1434.92, 1435.81, 1436.80, 1437.91])
+        sca_idx = 6
+        sld_idx = 2
+
+        test_result = find_cd_idx(sca_idx, sld_idx, test_array)
+        self.assertEqual(test_result, 10)
+
     #def test_soundchannelaxis(self):
 
 
