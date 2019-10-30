b9fa360ff575f0fc8e1db629460245fccc5f91c5
Noah Gallant <noah.gallant@mun.ca>
2019-10-30 15:29:57 -0300

    added more sscp tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -11,11 +11,22 @@ class TestFunctions(unittest.TestCase):
     
     def test_sscp_point(self):
         sspeed_1 = np.array([1434.08, 1435.48, 1436.10, 1435.16, 1433.01, 1432.80, 1432.54, 1432.75, 1433.20, 1434.92, 1435.81, 1436.80, 1437.91])
+        sspeed_2 = np.array([1436.17, 1435.52, 1434.81, 1433.56, 1433.90, 1434.18, 1434.75, 1435.00, 1435.63, 1435.99, 1436.28, 1436.61, 1437.12])
+        sspeed_3 = np.array([1437.32, 1436.70, 1435.45, 1433.97, 1436.16, 1435.27, 1433.81, 1433.11, 1433.79, 1434.98, 1436.29, 1437.16, 1438.12])
+        sspeed_4 = np.array([1435.76, 1434.12, 1434.98, 1436.27, 1434.51, 1433.97, 1433.49, 1433.67, 1435.09, 1435.98, 1436.12, 1437.41, 1438.19])
+        
         test_max = 13
 
-        test_result = sscp_point(sspeed_1, test_max)
-        self.assertEqual(test_result, 0)
-
+        result_1 = sscp_point(sspeed_1, test_max)
+        result_2 = sscp_point(sspeed_2, test_max)
+        result_3 = sscp_point(sspeed_3, test_max)
+        result_4 = sscp_point(sspeed_4, test_max)
+        
+        self.assertEqual(result_1, 0)
+        self.assertEqual(result_2, 0)
+        self.assertEqual(result_3, 1)
+        self.assertEqual(result_4, 1)
+        
     #def test_sscp(self):
 
 
