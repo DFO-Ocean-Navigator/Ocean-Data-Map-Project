0a7ab52764f58269eb56b9554769950d9b75635e
Noah Gallant <noah.gallant@mun.ca>
2019-11-04 11:21:32 -0400

    used unittest.skip instead of comments

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -121,42 +121,44 @@ class TestFunctions(unittest.TestCase):
         test_result = find_cd_idx(sca_idx, sld_idx, sspeed_4)
         self.assertAlmostEqual(test_result, 10)
         
-
-    #def test_cd_interpolation(self):
+    @unittest.skip('Test Not Working')
+    def test_cd_interpolation(self):
     
-        # Testing Typical sspeed profile
-        #sspeed_1 = np.array([1434.08, 1435.48, 1436.10, 1435.16, 1433.01, 1432.80, 1432.54, 1432.75, 1433.20, 1434.92, 1435.81, 1436.80, 1437.91])
-        #cd_idx = 10
-        #sld_idx = 2
-        #depth = np.array([0, 10, 30, 60, 100, 150, 210, 280, 360, 460, 560, 670, 790])
+         Testing Typical sspeed profile
+        sspeed_1 = np.array([1434.08, 1435.48, 1436.10, 1435.16, 1433.01, 1432.80, 1432.54, 1432.75, 1433.20, 1434.92, 1435.81, 1436.80, 1437.91])
+        cd_idx = 10
+        sld_idx = 2
+        depth = np.array([0, 10, 30, 60, 100, 150, 210, 280, 360, 460, 560, 670, 790])
         
-        #test_result = cd_interpolation(cd_idx, sld_idx, sspeed_1, depth)
-        #self.assertEqual(test_result, 10)
+        test_result = cd_interpolation(cd_idx, sld_idx, sspeed_1, depth)
+        self.assertEqual(test_result, 10)
     
-        # Testing immediately declining sspeed profile
-        #sspeed_2 = np.array([1436.17, 1435.52, 1434.81, 1433.56, 1433.90, 1434.18, 1434.75, 1435.00, 1435.63, 1435.99, 1436.28, 1436.61, 1437.12])
-        #cd_idx = 10
-        #Wsld_idx = 0
+         Testing immediately declining sspeed profile
+        sspeed_2 = np.array([1436.17, 1435.52, 1434.81, 1433.56, 1433.90, 1434.18, 1434.75, 1435.00, 1435.63, 1435.99, 1436.28, 1436.61, 1437.12])
+        cd_idx = 10
+        Wsld_idx = 0
 
-        #test_result = cd_interpolation(cd_idx, sld_idx, sspeed_2, depth)
-        #self.assertEqual(test_result, 10)
+        test_result = cd_interpolation(cd_idx, sld_idx, sspeed_2, depth)
+        self.assertEqual(test_result, 10)
 
-        #sspeed_3 = np.array([1437.32, 1436.70, 1435.45, 1433.97, 1436.16, 1435.27, 1433.81, 1433.11, 1433.79, 1434.98, 1436.29, 1437.16, 1438.12])
-        #sld_idx = 3
-        #cd_idx = 10
+        sspeed_3 = np.array([1437.32, 1436.70, 1435.45, 1433.97, 1436.16, 1435.27, 1433.81, 1433.11, 1433.79, 1434.98, 1436.29, 1437.16, 1438.12])
+        sld_idx = 3
+        cd_idx = 10
 
         
-        #sspeed_4 = np.array([1435.76, 1434.12, 1434.98, 1436.27, 1434.51, 1433.97, 1433.49, 1433.67, 1435.09, 1435.98, 1436.12, 1437.41, 1438.19])
+        sspeed_4 = np.array([1435.76, 1434.12, 1434.98, 1436.27, 1434.51, 1433.97, 1433.49, 1433.67, 1435.09, 1435.98, 1436.12, 1437.41, 1438.19])
         
+    @unittest.skip('Not Implemented')
+    def test_soniclayerdepth(self):
+        pass
 
-    #def test_soniclayerdepth(self):
+    @unittest.skip('Not Implemented')
+    def test_criticaldepth(self):
+        pass
 
-
-    #def test_criticaldepth(self):
-
-
-    #def test_depthexcess(self):
-
+    @unittest.skip('Not Implemented')
+    def test_depthexcess(self):
+        pass
 
     
         
