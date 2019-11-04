0e83b9e0304fbb9798dfc6b9842a59bd4e476dad
Noah Gallant <noah.gallant@mun.ca>
2019-11-04 11:23:32 -0400

    fixed uncommented comment

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -124,7 +124,7 @@ class TestFunctions(unittest.TestCase):
     @unittest.skip('Test Not Working')
     def test_cd_interpolation(self):
     
-         Testing Typical sspeed profile
+        # Testing Typical sspeed profile
         sspeed_1 = np.array([1434.08, 1435.48, 1436.10, 1435.16, 1433.01, 1432.80, 1432.54, 1432.75, 1433.20, 1434.92, 1435.81, 1436.80, 1437.91])
         cd_idx = 10
         sld_idx = 2
@@ -133,7 +133,7 @@ class TestFunctions(unittest.TestCase):
         test_result = cd_interpolation(cd_idx, sld_idx, sspeed_1, depth)
         self.assertEqual(test_result, 10)
     
-         Testing immediately declining sspeed profile
+        # Testing immediately declining sspeed profile
         sspeed_2 = np.array([1436.17, 1435.52, 1434.81, 1433.56, 1433.90, 1434.18, 1434.75, 1435.00, 1435.63, 1435.99, 1436.28, 1436.61, 1437.12])
         cd_idx = 10
         Wsld_idx = 0
