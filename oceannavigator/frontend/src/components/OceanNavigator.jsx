5e62e8977320f3727c1650c804d93fc3a80b6c24
Noah Gallant <noah.gallant@mun.ca>
2019-11-08 12:46:12 -0400

    hide modal

--- oceannavigator/frontend/src/components/OceanNavigator.jsx
+++ oceannavigator/frontend/src/components/OceanNavigator.jsx
@@ -696,7 +696,7 @@ export default class OceanNavigator extends React.Component {
             updateLanguage={this.updateLanguage}
           />
           <WarningBar
-            showWarningInfo={this.showBugsModal}
+            showWarningInfo={false}
           />
           {map}
         </div>
