953b4ecf3baf1915b2749d6722a96da29fefd59e
Noah Gallant <noah.gallant@mun.ca>
2019-11-15 16:20:48 -0400

    fixed indentation

--- plotting/bathymetry.py
+++ plotting/bathymetry.py
@@ -571,48 +571,48 @@ class BathPlotter(Plotter):
         
 
         rng = pd.date_range('1/1/2011', periods=7500, freq='H')
-    ts = pd.Series(np.random.randn(len(rng)), index=rng)
+        ts = pd.Series(np.random.randn(len(rng)), index=rng)
 
-    graphs = [
-        dict(
-            data=[
-                dict(
-                    x=[1, 2, 3],
-                    y=[10, 20, 30],
-                    type='scatter'
-                ),
-            ],
-            layout=dict(
-                title='first graph'
-            )
-        ),
-
-        dict(
-            data=[
-                dict(
-                    x=[1, 3, 5],
-                    y=[10, 50, 30],
-                    type='bar'
-                ),
-            ],
-            layout=dict(
-                title='second graph'
-            )
-        ),
-
-        dict(
-            data=[
-                dict(
-                    x=ts.index,  # Can use the pandas data structures directly
-                    y=ts
+        graphs = [
+            dict(
+                data=[
+                    dict(
+                        x=[1, 2, 3],
+                        y=[10, 20, 30],
+                        type='scatter'
+                    ),
+                ],
+                layout=dict(
+                    title='first graph'
                 )
-            ]
-        )
-    ]
+            ),
 
-    # Add "ids" to each of the graphs to pass up to the client
-    # for templating
-    ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
+            dict(
+                data=[
+                    dict(
+                        x=[1, 3, 5],
+                        y=[10, 50, 30],
+                        type='bar'
+                    ),
+                ],
+                layout=dict(
+                    title='second graph'
+                )
+            ),
+
+            dict(
+                data=[
+                    dict(
+                        x=ts.index,  # Can use the pandas data structures directly
+                        y=ts
+                    )
+                ]
+            )
+        ]
+
+        # Add "ids" to each of the graphs to pass up to the client
+        # for templating
+        ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
         graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
 
 
