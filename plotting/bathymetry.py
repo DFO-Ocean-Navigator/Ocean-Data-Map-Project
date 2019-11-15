14cdc07fd035fad2b6c65791ef2ccd7cce6aaa4b
Noah Gallant <noah.gallant@mun.ca>
2019-11-15 16:20:01 -0400

    test

--- plotting/bathymetry.py
+++ plotting/bathymetry.py
@@ -570,12 +570,52 @@ class BathPlotter(Plotter):
         
         
 
-        a = np.linspace(start=0, stop=36, num=36)
-        np.random.seed(25)
-        b = np.random.uniform(low=0.0, high=1.0, size=36)
+        rng = pd.date_range('1/1/2011', periods=7500, freq='H')
+    ts = pd.Series(np.random.randn(len(rng)), index=rng)
 
-        trace = go.Scatter(x=a, y=b)
-        data = [trace]
-        pyplot.iplot(data, file='basic-line-chart')
+    graphs = [
+        dict(
+            data=[
+                dict(
+                    x=[1, 2, 3],
+                    y=[10, 20, 30],
+                    type='scatter'
+                ),
+            ],
+            layout=dict(
+                title='first graph'
+            )
+        ),
 
-        return
+        dict(
+            data=[
+                dict(
+                    x=[1, 3, 5],
+                    y=[10, 50, 30],
+                    type='bar'
+                ),
+            ],
+            layout=dict(
+                title='second graph'
+            )
+        ),
+
+        dict(
+            data=[
+                dict(
+                    x=ts.index,  # Can use the pandas data structures directly
+                    y=ts
+                )
+            ]
+        )
+    ]
+
+    # Add "ids" to each of the graphs to pass up to the client
+    # for templating
+    ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
+        graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+        return render_template('layouts/index.html',
+                                ids=ids, 
+                                graphJSON=graphJSON)
