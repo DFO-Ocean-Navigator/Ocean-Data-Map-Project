ada97020294973ec49ca6698a1375fd355105056
Noah Gallant <noah.gallant@mun.ca>
2019-11-16 20:15:17 -0400

    test

--- plotting/bathymetry.py
+++ plotting/bathymetry.py
@@ -31,9 +31,10 @@ from plotting.plotter import Plotter
 from utils.errors import ClientError, ServerError
 from utils.misc import list_areas
 from flask import render_template
-import chart_studio.plotly as plotly
-import chart_studio.utils as csutils
-import pandas as pd
+
+# New PLOTLY imports
+from plotly.offline import plot
+from plotly.graph_objs import Scatter
 
 class BathPlotter(Plotter):
 
@@ -572,52 +573,4 @@ class BathPlotter(Plotter):
         
         
 
-        rng = pd.date_range('1/1/2011', periods=7500, freq='H')
-        ts = pd.Series(np.random.randn(len(rng)), index=rng)
-
-        graphs = [
-            dict(
-                data=[
-                    dict(
-                        x=[1, 2, 3],
-                        y=[10, 20, 30],
-                        type='scatter'
-                    ),
-                ],
-                layout=dict(
-                    title='first graph'
-                )
-            ),
-
-            dict(
-                data=[
-                    dict(
-                        x=[1, 3, 5],
-                        y=[10, 50, 30],
-                        type='bar'
-                    ),
-                ],
-                layout=dict(
-                    title='second graph'
-                )
-            ),
-
-            dict(
-                data=[
-                    dict(
-                        x=ts.index,  # Can use the pandas data structures directly
-                        y=ts
-                    )
-                ]
-            )
-        ]
-
-        # Add "ids" to each of the graphs to pass up to the client
-        # for templating
-        ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
-        graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-
-
-        return render_template('layouts/index.html',
-                                ids=ids, 
-                                graphJSON=graphJSON)
+        return 
