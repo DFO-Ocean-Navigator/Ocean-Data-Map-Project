854f36a678de0893e1993e2b8e7ce6c6e37820a3
Noah Gallant <noah.gallant@mun.ca>
2019-11-19 15:08:11 -0400

    moved to non inline style

--- oceannavigator/frontend/src/components/OceanNavigator.jsx
+++ oceannavigator/frontend/src/components/OceanNavigator.jsx
@@ -705,7 +705,6 @@ export default class OceanNavigator extends React.Component {
           show={this.state.showModal}
           onHide={this.closeModal}
           dialogClassName='full-screen-modal'
-          style={{height: '100%'}}
           backdrop={true}
         >
           <Modal.Header closeButton closeLabel={_("Close")}>
