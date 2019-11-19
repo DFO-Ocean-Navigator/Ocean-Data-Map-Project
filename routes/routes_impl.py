5fb971b1aebb8d841906ac8ccc0de6c85e902216
Noah Gallant <noah.gallant@mun.ca>
2019-11-19 09:43:59 -0400

    removed unecessary stuff

--- routes/routes_impl.py
+++ routes/routes_impl.py
@@ -833,12 +833,6 @@ def bath_3d_model_impl(query: dict, args):
     """
 
     dataset = query.get('dataset')
-    
-    options = {}
-    options['format'] = fmt
-    options['size'] = args.get('size', '15x9')
-    options['dpi'] = args.get('dpi', 72)
-
 
     plotter = BathPlotter(dataset, query, **options)
     return plotter.run()
