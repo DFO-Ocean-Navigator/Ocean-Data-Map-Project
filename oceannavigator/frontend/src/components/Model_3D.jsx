c519929329b2f3c9d67075162bb7e0cf57195f40
Noah Gallant <noah.gallant@mun.ca>
2019-11-20 11:36:05 -0400

    test

--- oceannavigator/frontend/src/components/Model_3D.jsx
+++ oceannavigator/frontend/src/components/Model_3D.jsx
@@ -20,7 +20,7 @@ export default class Model_3D extends React.Component {
                 "time": this.props.time,
             },
             dataset: this.props.dataset,
-            variables: this.props.variables
+            variables: this.props.variable
         }
     
         this.urlFromStateQuery = this.urlFromStateQuery.bind(this);
@@ -31,6 +31,7 @@ export default class Model_3D extends React.Component {
 
     componentDidMount() {
         this.loadNextPlot();
+        this._mounted = true;
     }
 
     updateVariables(values) {
@@ -70,72 +71,77 @@ export default class Model_3D extends React.Component {
     }
 
     render() {
-
+        let content = []
         // Create all the components individually that need to be added to panels
+        if (this._mounted) {
+            const select_dataset = (
+                <ComboBox
+                  key='dataset'
+                  id='dataset'
+                  state={this.state.dataset}
+                  def=''
+                  url='/api/v1.0/datasets/'
+                  title={_("Dataset")}
+                  onUpdate={this.updateDataset}
+                />
+            )
+    
+            const select_variable = (
+                <ComboBox
+                  id='variable'
+                  key='variable'
+                  multiple={true}
+                  state={this.state.variables}
+                  def={"defaults.dataset"}
+                  onUpdate={this.updateVariables}
+                  url={"/api/v1.0/variables/?vectors&dataset=" + this.state.dataset
+                  }
+                  title={_("Variables")}
+                />
+            )
+            
+            const toggle_apply = (
+                <Button
+                    onClick={this.loadNextPlot}
+                >Apply</Button>
+            )
+    
+            // Create Arrays holding all the components for each panel
+            let data_selection = [select_dataset, select_variable, toggle_apply];
+    
+            // Create the panels if their associated arrays are not of length 0
+            <Panel
+              key='right_map'
+              id='right_map'
+              collapsible
+              defaultExpanded
+              header={_("Right Map")}
+              bsStyle='primary'
+            >
+                {data_selection}
+            </Panel>
+    
+            // Load Plot to render
+    
+            let plot = <iframe src={this.state.url} frameBorder="0" style={{width: '100%', height:'100%'}}></iframe>
         
-        const select_dataset = (
-            <ComboBox
-              key='dataset'
-              id='dataset'
-              state={this.state.dataset}
-              def=''
-              url='/api/v1.0/datasets/'
-              title={_("Dataset")}
-              onUpdate={this.updateDataset}
-            />
-        )
-
-        const select_variable = (
-            <ComboBox
-              id='variable'
-              key='variable'
-              multiple={true}
-              state={this.state.variables}
-              def={"defaults.dataset"}
-              onUpdate={this.updateVariables}
-              url={"/api/v1.0/variables/?vectors&dataset=" + this.state.dataset
-              }
-              title={_("Variables")}
-            />
-        )
+            content = <Row style={{height: '100%'}}>
+                <Col lg={2} style={{height: '100%'}}>
+                  {data_selection}
+                </Col>
+                <Col lg={8} style={{height: '100%'}}>
+                  <div style={{height: '100%', width: '100%'}}>
+                      {plot}
+                  </div>
+                </Col>
+            </Row>
+        }
         
-        const toggle_apply = (
-            <Button
-                onClick={this.loadNextPlot}
-            >Apply</Button>
-        )
-
-        // Create Arrays holding all the components for each panel
-        let data_selection = [select_dataset, select_variable];
-
-        // Create the panels if their associated arrays are not of length 0
-        <Panel
-          key='right_map'
-          id='right_map'
-          collapsible
-          defaultExpanded
-          header={_("Right Map")}
-          bsStyle='primary'
-        >
-            {data_selection}
-        </Panel>
-
-        // Load Plot to render
-
-        let plot = <iframe src={this.state.url} frameBorder="0" style={{width: '100%', height:'100%'}}></iframe>
 
         return (
-            <Row style={{height: '100%'}}>
-              <Col lg={2} style={{height: '100%'}}>
-                {data_selection}
-              </Col>
-              <Col lg={8} style={{height: '100%'}}>
-                <div style={{height: '100%', width: '100%'}}>
-                    {plot}
-                </div>
-              </Col>
-            </Row>
-            
+            <div>
+                {content}
+            </div>
         )
     }
 }
