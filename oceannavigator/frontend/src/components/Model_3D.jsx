9c0cc874cfdf0b604885baa5b0675eb4313945dd
Noah Gallant <noah.gallant@mun.ca>
2019-11-21 11:19:40 -0400

    test

--- oceannavigator/frontend/src/components/Model_3D.jsx
+++ oceannavigator/frontend/src/components/Model_3D.jsx
@@ -92,6 +92,9 @@ export default class Model_3D extends React.Component {
     }
 
     updateDataset(key, value) {
+        if (value === 'string') {
+            return
+        }
         console.warn("UPDATING DATASET:", key, value);
     
         let datasets = this.state.next_query.datasets;
