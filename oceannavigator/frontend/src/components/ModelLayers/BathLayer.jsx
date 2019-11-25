74b13bac4ec58359affba99384b263c331bcac31
Noah Gallant <noah.gallant@mun.ca>
2019-11-25 16:37:01 -0400

    test

--- oceannavigator/frontend/src/components/ModelLayers/BathLayer.jsx
+++ oceannavigator/frontend/src/components/ModelLayers/BathLayer.jsx
@@ -31,21 +31,24 @@ export default class BathLayer extends React.Component {
             radius: this.props.radius
         }
         let self = this
+
+        let old = self.state.surface;
+        let layer = self.state.surface;
+        if (old === undefined) {
+            layer = {
+                z: [],
+                type: 'surface',
+                colorscale: 'Earth',
+            }
+        }
+
         $.ajax({
             type: 'GET',
             dataType: 'json',
             url: this.props.urlFromQuery('/api/v1.0/data/bathymetry/', query),
             success: function(result) {
                 console.warn("STATE: ", this.state);
-                let old = self.state.surface;
-                let layer = self.state.surface;
-                if (old === undefined) {
-                    layer = {
-                        z: [],
-                        type: 'surface',
-                        colorscale: 'Earth',
-                    }
-                }
+                
                 layer = jQuery.extend({}, layer);
                 layer.z = result;
                 self.setState({
@@ -59,20 +62,12 @@ export default class BathLayer extends React.Component {
         $.ajax({
             type: 'GET',
             dataType: 'json',
-            url: this.props.urlFromQuery('/api/v1.0/data/bathymetry/', query),
+            url: this.props.urlFromQuery('/api/v1.0/data/latlon/', query),
             success: function(result) {
                 console.warn("STATE: ", this.state);
-                let old = self.state.surface;
-                let layer = self.state.surface;
-                if (old === undefined) {
-                    layer = {
-                        z: [],
-                        type: 'surface',
-                        colormap: 'Earth',
-                    }
-                }
                 layer = jQuery.extend({}, layer);
-                layer.z = result;
+                layer.x = result[0];
+                layer.y = result[1];
                 self.setState({
                     data: result,
                     surface: layer
