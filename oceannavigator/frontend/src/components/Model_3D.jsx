78b37004c15f96fff5244eee523e14fc4f4d23a3
Noah Gallant <noah.gallant@mun.ca>
2019-11-25 11:51:20 -0400

    test

--- oceannavigator/frontend/src/components/Model_3D.jsx
+++ oceannavigator/frontend/src/components/Model_3D.jsx
@@ -53,7 +53,7 @@ export default class Model_3D extends React.Component {
         Updates the specified data with the provided data
     */
     updateDataLayer(idx, layer) {
-        let layers = this.state.layers;
+        let layers = $.extend({}, this.state.layers);
         
         if (idx === undefined) {
             layers.push(layer);
