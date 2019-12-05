29c93914f3d546f9cf070a37fd042c939e72298e
Noah Gallant <noah.gallant@mun.ca>
2019-12-05 00:23:33 -0400

    Fixed import statement

--- oceannavigator/frontend/src/components/Model_3D.jsx
+++ oceannavigator/frontend/src/components/Model_3D.jsx
@@ -5,7 +5,7 @@ import SelectBox from "./SelectBox.jsx";
 import Plot from 'react-plotly.js';
 import DataLayer from './ModelLayers/DataLayer.jsx';
 import BathLayer from './ModelLayers/BathLayer.jsx';
-import LocationInpute from './LocationInput.jsx';
+import LocationInput from './LocationInput.jsx';
 
 const stringify = require("fast-stable-stringify");
 const i18n = require("../i18n.js");
