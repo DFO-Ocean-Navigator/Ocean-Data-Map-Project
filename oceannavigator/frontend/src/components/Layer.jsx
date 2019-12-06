506a46c8e53003e800a7cf754c96e0f21fa85a30
Noah Gallant <noah.gallant@mun.ca>
2019-12-06 17:14:38 -0500

    test

--- oceannavigator/frontend/src/components/Layer.jsx
+++ oceannavigator/frontend/src/components/Layer.jsx
@@ -755,8 +755,8 @@ export default class Layer extends React.Component {
 
     layer.set('scaleBar', jQuery.extend({}, scaleBar))
     if (this.props.datasetconfig !== undefined) {
-      new_layer.set('dataset', this.props.datasetconfig[this.state.current_dataset].name);
-      new_layer.set('variable', this.props.datasetconfig[this.state.current_dataset].variables[this.state.current_variable].name);  
+      layer.set('dataset', this.props.datasetconfig[this.state.current_dataset].name);
+      layer.set('variable', this.props.datasetconfig[this.state.current_dataset].variables[this.state.current_variable].name);  
     }
 
     let props = layer.getSource().getProperties();
