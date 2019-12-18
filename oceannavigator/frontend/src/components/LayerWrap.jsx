9e4b987cf4ae8b02cc8186fc04a2f2ef73e6e98f
Noah Gallant <noah.gallant@mun.ca>
2019-12-18 15:44:00 -0400

    Removed unused code, corrected formatting

--- oceannavigator/frontend/src/components/LayerWrap.jsx
+++ oceannavigator/frontend/src/components/LayerWrap.jsx
@@ -67,7 +67,7 @@ export default class LayerWrap extends React.Component {
     if (new_timeSources !== undefined && new_timeSources[map] !== undefined && new_timeSources[map][this.props.layerType] !== undefined) {
       if (dataset in new_timeSources[map][this.props.layerType]) {
         if (new_timeSources[map][this.props.layerType][dataset]['variables'].includes(variable)) {
-          
+
           let idx = new_timeSources[map][this.props.layerType][dataset]['variables'].indexOf(variable)
           new_timeSources[map][this.props.layerType][dataset]['variables'].splice(idx, 1)
         }
@@ -83,119 +83,102 @@ export default class LayerWrap extends React.Component {
   }
 
 
-removeData(map, dataset, variable, idx) {
-  // Data Stored as
-  // map: {
-  //   layer: {
-  //     dataset: {
-  //       variables: {
-  //         frequency: #,
-  //         data: ...
-  //         data: ...
-  //         data: ...
-  //       }
-  //     }
-  //   }
-  // }
-  return
-  if (map === undefined || dataset === undefined || variable === undefined || idx === undefined) {
+  removeData(map, dataset, variable, idx) {
+    // Data Stored as
+    // map: {
+    //   layer: {
+    //     dataset: {
+    //       variables: {
+    //         frequency: #,
+    //         data: ...
+    //         data: ...
+    //         data: ...
+    //       }
+    //     }
+    //   }
+    // }
     return
-  }
+    if (map === undefined || dataset === undefined || variable === undefined || idx === undefined) {
+      return
+    }
 
-  let data = this.props.state.data[map][this.props.layerType][idx]
-  let full_data
-  if (! data.includes(dataset) || jQuery.isEmptyObject(data[dataset])) {
-    return
-  } else {
-    data[dataset] = undefined
-    delete data[dataset]
-    
-    if (jQuery.isEmptyObject(data)) {
-      data = this.props.state.data[map][this.props.layerType]
-      data[idx] = undefined
-      delete data[idx]
+    let data = this.props.state.data[map][this.props.layerType][idx]
+    let full_data
+    if (!data.includes(dataset) || jQuery.isEmptyObject(data[dataset])) {
+      return
+    } else {
+      data[dataset] = undefined
+      delete data[dataset]
 
       if (jQuery.isEmptyObject(data)) {
-        data = this.props.state.data[map]
-        data[this.props.layerType] = undefined
-        delete data[this.props.layerType]
+        data = this.props.state.data[map][this.props.layerType]
+        data[idx] = undefined
+        delete data[idx]
 
         if (jQuery.isEmptyObject(data)) {
-          data = this.props.state.data
-          data[map] = undefined
-          delete data[map]
+          data = this.props.state.data[map]
+          data[this.props.layerType] = undefined
+          delete data[this.props.layerType]
+
+          if (jQuery.isEmptyObject(data)) {
+            data = this.props.state.data
+            data[map] = undefined
+            delete data[map]
+          } else {
+            full_data = this.props.state.data
+            full_data[map] = data
+          }
         } else {
           full_data = this.props.state.data
-          full_data[map] = data
+          full_data[map][this.props.layerType] = data
         }
       } else {
         full_data = this.props.state.data
-        full_data[map][this.props.layerType] = data
+        full_data[map][this.props.layerType][idx] = data
       }
-    } else {
-      full_data = this.props.state.data
-      full_data[map][this.props.layerType][idx] = data
     }
-  } 
-  this.props.globalUpdate('data', jQuery.extend({}, full_data))
-}
-
-render() {
-  let layers = []
-  for (let idx in this.state.layers) {
-    layers.push(<Layer
-      datasetconfig={this.props.datasetconfig}
-      index={idx}
-      key={idx}
-      value={idx}
-      state={this.props.state}
-      layers={this.props.state.layers}
-      removeLayer={this.removeLayer}
-      removeData={this.removeData}
-      mapComponent={this.props.mapComponent}  // Left map Component
-      mapComponent2={this.props.mapComponent2} // Left map component - for dataset compare
-      globalUpdate={this.props.globalUpdate}
-      options={this.props.state.options}
-      layerType={this.props.layerType}
-      layerName={this.props.layerName}
-      //swapViews={this.props.swapViews}
-      showHelp={this.props.showHelp}
-      updateOptions={this.props.updateOptions}
-    //defaultDataset='giops_day'
-    //defaultVariable='u-component_of_wind_height_above_ground'
-    ></Layer>)
+    this.props.globalUpdate('data', jQuery.extend({}, full_data))
   }
 
+  render() {
+    let layers = []
+    for (let idx in this.state.layers) {
+      layers.push(<Layer
+        datasetconfig={this.props.datasetconfig}
+        index={idx}
+        key={idx}
+        value={idx}
+        state={this.props.state}
+        layers={this.props.state.layers}
+        removeLayer={this.removeLayer}
+        removeData={this.removeData}
+        mapComponent={this.props.mapComponent}  // Left map Component
+        mapComponent2={this.props.mapComponent2} // Left map component - for dataset compare
+        globalUpdate={this.props.globalUpdate}
+        options={this.props.state.options}
+        layerType={this.props.layerType}
+        layerName={this.props.layerName}
+        //swapViews={this.props.swapViews}
+        showHelp={this.props.showHelp}
+        updateOptions={this.props.updateOptions}
+      //defaultDataset='giops_day'
+      //defaultVariable='u-component_of_wind_height_above_ground'
+      ></Layer>)
+    }
 
-  
-  return (
-    <div>
 
-      {/*<Layer
-              state={this.props.state}
-              swapViews={this.props.swapViews}
-              toggleLayer={this.props.toggleLayer}
-              reloadLayer={this.props.reloadLayer}
-              mapComponent={this.props.mapComponent}
-              globalUpdate={this.props.globalUpdate}
-              showHelp={this.props.showHelp}
-              options={this.props.state.options}
-              updateOptions={this.props.updateOptions}
-              layerType='met'
-              defaultDataset='gem'
-            />*/}
-      {layers}
-      <Button
-        onClick={this.addLayer}
-      >
 
-        New {this.props.layerType} Layer
-
-            </Button>
-
-    </div>
-  );
-}
+    return (
+      <div>
+        {layers}
+        <Button
+          onClick={this.addLayer}
+        >
+          New {this.props.layerType} Layer
+      </Button>
+      </div>
+    );
+  }
 }
 
 //***********************************************************************
