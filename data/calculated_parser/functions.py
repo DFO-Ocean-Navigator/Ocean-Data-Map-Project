Fix incorrect sound channel calculation

data/calculated_parser/functions.py
@@ -134,7 +134,8 @@ def tempgradient(depth, latitude, temperature, salinity):
 
 def deepsoundchannel(depth, latitude, temperature, salinity):
     """
-    Finds the minimum of the speed of sound.
+    Find and return the depth of the minimum value of the
+    speed of sound.
 
     # https://en.wikipedia.org/wiki/SOFAR_channel
 
@@ -147,12 +148,17 @@ def deepsoundchannel(depth, latitude, temperature, salinity):
 
     sound_speed = sspeed(depth, latitude, temperature, salinity)
 
+        # Mask out NaN values to prevent a exception blow-up.
+    masked_sound_speed = np.ma.masked_array(sound_speed, np.isnan(sound_speed))
+
     # The resulting shape of sound_speed is (full_depth, lat_slice, lon_slice)
     # So we simply need to find the minimum sound speed along each lat/lon index
     # Using numpy axis magic, we want the minimum along the full_depth axis = 0.
     # https://stackoverflow.com/a/52468964/2231969
+    
+    min_indices = np.argmin(masked_sound_speed, axis=0)
 
-    return np.nanmin(sound_speed, axis=0)
+    return depth.values[min_indices]
 
 
 def _metpy(func, data, lat, lon, dim):