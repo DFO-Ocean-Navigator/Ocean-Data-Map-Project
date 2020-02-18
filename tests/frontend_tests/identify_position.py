db6dee3179adb13319ba7537de136ace21fe3c23
Samuel <tsb050@mun.ca>
2020-02-18 15:20:30 -0330

    Auxiliiary script to identify x and y position of mouse

---
+++ tests/frontend_tests/identify_position.py
@@ -0,0 +1,31 @@
+"""
+Print x and y location of mouse position
+==========================
+:Author: Samuel Babalola
+:Created: 2020-02-11
+
+This module creates a yaml file containing x and y 
+dimensions of the screen for mouse projections.
+"""
+
+import os
+import pyautogui as gui
+import time
+import yaml
+
+# Identify mouse postion
+def identifyloc():
+    """
+    Locate and return current x and y mouse postion
+    """
+    while True:
+        MouseX, MouseY = gui.position()
+        print(MouseX, MouseY)
+        time.sleep(2.3)
+
+
+def main():
+    identifyloc()
+
+if '__main__' == __name__:
+    main()
