719a3d2e62eb30491e54c2e2fd27d2e83a8dfa68
Dwayne Hart <dwayne.hart@gmail.com>
2020-03-02 17:25:25 -0330

    Altered two scripts so that the user does not have to define a port to use. Also changed the number of gunicorn instances to 4.

--- launch-web-service.sh
+++ launch-web-service.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
+HOST_IP=`ip addr show | egrep "e(th|np)" | grep inet | awk '{print $2}' | sed -e 's/\/[0-9]*//'`
+
+PORT=5000
+
+nc -zv 0.0.0.0 $PORT > /dev/null
+RES=$?
+
+while  [ $((RES)) -eq 0 ] ; do
+	PORT=$((PORT+1))
+	nc -zv 0.0.0.0 $PORT > /dev/null
+	RES=$?
+done
+
+echo "Use the following IP in your URL ${HOST_IP}:$((PORT))"
 echo "This will log information from the application to the screen and the logfile."
-exec bash runserver.sh &> >(tee -a /home/buildadm/launch-web-service.log)
+exec bash runserver.sh $PORT &> >(tee -a ${HOME}/launch-on-web-service.log)
