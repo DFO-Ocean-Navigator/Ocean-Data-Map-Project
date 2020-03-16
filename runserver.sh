d2195fd03fb8d16e694c96bcc01d2dfb20e6ee18
dwayne-hart <44408505+dwayne-hart@users.noreply.github.com>
2020-03-16 17:15:14 -0230

    Update to the runserver.sh script (#681)
    * Launch as many gunicorn processes as their are cores. The timeout was increased for both the silent works terminated and restated. And again for the graceful retart of workers.
    
    * Implemented change from codefactor-io... Useless cat. Consider 'cmd < file | ..' or 'cmd file | ..' instead. (SC2002)

--- runserver.sh
+++ runserver.sh
@@ -5,4 +5,7 @@
 #                                       Argument not required.
 
 [[ x"$1" == x"" ]] && PORT=5000 || PORT=$1
-gunicorn -w 4 -t 90 --graceful-timeout 90 --preload -b 0.0.0.0:$((PORT)) --reload "oceannavigator:create_app()" $2
+
+NUMBER_PROCS=$(awk /processor/'{processor++} END {print processor}' < /proc/cpuinfo)
+
+gunicorn -w $((NUMBER_PROCS)) -t 300 --graceful-timeout 300 --preload -b 0.0.0.0:$((PORT)) --reload "oceannavigator:create_app()" $2
