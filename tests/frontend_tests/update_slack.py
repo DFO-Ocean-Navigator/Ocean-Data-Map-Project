71cf0c6c3d359bcf04e79c3f3c6ad0a919d48bfe
Samuel <tsb050@mun.ca>
2020-03-19 15:03:01 -0230

    Write UI test results to slack channel

---
+++ tests/frontend_tests/update_slack.py
@@ -0,0 +1,65 @@
+"""
+Write test results to slack channel ui_tests
+==========================
+:Author: Samuel Babalola
+:Created: 2020-03-11
+
+"""
+
+import os
+import slack
+
+from dimension_config import open_config
+
+# The slack library requires a defined pattern of writing
+# messages. This is what this log_to_channel function does
+# It rearranges the results from test_results.yaml to fit
+# the index function below.
+
+
+def log_to_channel(log_file):
+    config = open_config(log_file)
+    message = config['Test results']
+
+    list_result = []
+    for tests in message:
+        if type(message[tests]) is str:
+            result_format = '{} : {}'.format(tests, message[tests])
+            result_index = index(result_format)
+            list_result.append(result_index)
+            continue
+
+        for result in message[tests]:
+            result_format = '{} - {} : {}'.format(
+                tests, result, message[tests][result])
+            result_index = index(result_format)
+            list_result.append(result_index)
+
+    return list_result
+
+
+def index(input):
+    blocks = {
+        "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": input
+                }
+    }
+    return blocks
+
+
+def main():
+    results = log_to_channel('test_results.yaml')
+    slack_config = open_config('slack_token.yaml')
+    slack_token = slack_config['token']
+    client = slack.WebClient(token=slack_token)
+
+    client.chat_postMessage(
+        channel="ui-tests",
+        blocks=results
+    )
+
+
+if '__main__' == __name__:
+    main()
