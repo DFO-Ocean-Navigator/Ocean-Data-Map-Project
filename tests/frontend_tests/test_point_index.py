9f91c566191b962993b9e7d7a0fc06efb5ca985d
Samuel <tsb050@mun.ca>
2020-04-02 12:51:42 -0230

    Updated configuration file calls, updated log functions

--- tests/frontend_tests/test_point_index.py
+++ tests/frontend_tests/test_point_index.py
@@ -8,13 +8,14 @@ This module contains functions for different tests
 of the user interface of the navigator.
 """
 
-
+import copy
+from datetime import datetime
 import pyautogui as gui
 import time
 import yaml
 
 from dimension_config import (open_config, write_to_config)
-from utils import (navigator_webpage, test_profile, 
+from utils import (navigator_webpage, test_profile, get_time, 
                    retry_location_test, move_et_click)
 
 
@@ -24,6 +25,9 @@ config = open_config()
 dimension = config['location']
 paths = config['paths']
 address = config['web_addresses']
+duration = config['duration']
+
+# Log profile results to a dictionary
 point_index_results = {
     'Profile' : None,
     'CTD Profile test' : None,
@@ -31,11 +35,12 @@ point_index_results = {
     'Sound Speed profile' : None,
     'Virtual_Mooring' : None
 }
+point_index_times = copy.deepcopy(point_index_results)
 
 # Set default sleep time
-sleep = 5
-plot_render_sleep = 10
-box_timeout = 2500
+sleep = duration['sleep']
+plot_render_sleep = duration['plot_render']
+box_timeout = duration['box_timeout']
 
 def draw_point():
     """
@@ -66,25 +71,26 @@ def draw_point():
     if image_loc is None:
         gui.alert(text='Point index not found!', title='Map point', button='OK', timeout=box_timeout)
         # Retry the test in case of slow network connection
-        retry_location_test(paths['point_index'], 'Map point')
-        result = 'Test Failed'
+        result = retry_location_test(paths['point_index'], 'Map point')
     else:
         time.sleep(0)
         gui.alert(text='Point UI test complete!', title='Map point', button='Close', timeout=box_timeout)
         result = 'Test Completed'
     point_index_results['Profile'] = result
-    point_tests(paths['point_index'], point_index_results)
+    point_index_times['Profile'] = get_time() 
+    point_tests(paths['point_index'], point_index_results, point_index_times)
     # Close index sub-tab
     time.sleep(.30)
     gui.click(dimension['close_index'])
     time.sleep(.30)
-    return point_index_results
+    return point_index_results, point_index_times
 
-def point_tests(test_compare, log):
-    ctd_profile = test_profile(test_compare, 'CTD Profile test', dimension['CTD_Profile'], log)
-    ts_diagram = test_profile(test_compare, 'T/S Diagram test', dimension['T/S_Diagram'], log)
-    sound_speed = test_profile(test_compare, 'Sound Speed profile', dimension['Sound_Speed'], log)
-    virtual_mooring = test_profile(test_compare, 'Virtual_Mooring', dimension['Virtual_Mooring'], log)
+
+def point_tests(test_compare, log_result, log_time):
+    ctd_profile = test_profile(test_compare, 'CTD Profile test', dimension['CTD_Profile'], log_result, log_time)
+    ts_diagram = test_profile(test_compare, 'T/S Diagram test', dimension['T/S_Diagram'], log_result, log_time)
+    sound_speed = test_profile(test_compare, 'Sound Speed profile', dimension['Sound_Speed'], log_result, log_time)
+    virtual_mooring = test_profile(test_compare, 'Virtual_Mooring', dimension['Virtual_Mooring'], log_result, log_time)
     
 
 def main():
--- tests/frontend_tests/test_temperature.py
+++ tests/frontend_tests/test_temperature.py
@@ -4,17 +4,17 @@ Perform UI test on the Ocean Navigator
 :Author: Samuel Babalola
 :Created: 2020-02-11
 
-This module contains functions for different tests
-of the user interface of the navigator.
+This module finds the temperature colour 
+of the ocean navigator navigator.
 """
 
-
+from datetime import datetime
 import pyautogui as gui
 import time
 import yaml
 
 from dimension_config import (open_config, write_to_config)
-from utils import (navigator_webpage, 
+from utils import (navigator_webpage, get_time, 
                       retry_location_test, move_et_click)
 
 #Open configuration file
@@ -23,11 +23,14 @@ config = open_config()
 dimension = config['location']
 paths = config['paths']
 address = config['web_addresses']
+duration = config['duration']
 
 # Set default sleep time
-sleep = 5
-plot_render_sleep = 10
-box_timeout = 2500
+
+sleep = duration['sleep']
+plot_render_sleep = duration['plot_render']
+box_timeout = duration['box_timeout']
+
 
 def find_temperature_bar():
     """
@@ -52,7 +55,7 @@ def find_temperature_bar():
         gui.click(button='right', x=image_loc.x, y=image_loc.y)
         gui.alert(text='Temperature bar check complete!', title='Temperature bar', button='Close', timeout=box_timeout)
         result = 'Test Completed'
-    return result
+    return result, get_time()
 
 def main():
     navigator_webpage()
