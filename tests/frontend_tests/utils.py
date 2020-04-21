c3298b9193f2235caf0d29a25276ee65feab82a4
Samuel <tsb050@mun.ca>
2020-04-21 10:56:42 -0230

    Remove cache form browser

--- tests/frontend_tests/utils.py
+++ tests/frontend_tests/utils.py
@@ -27,13 +27,12 @@ plot_render_sleep = duration['plot_render']
 box_timeout = duration['box_timeout']
 
 
-
 def navigator_webpage():
     """
 
     Function to access the navigator web page
     *reduce duplicated codes. 
-    
+
     """
     # Start firefox
     gui.press('winleft')
@@ -46,7 +45,7 @@ def navigator_webpage():
     gui.moveTo(dimension['firefox_search'])
     gui.typewrite(address['ocean_navigator'], interval=0.08)
     gui.press('enter')
-    time.sleep(sleep) 
+    time.sleep(sleep)
 
 
 def move_et_click(position):
@@ -54,7 +53,7 @@ def move_et_click(position):
 
     Function to move to a postion and click
     *reduce duplicated codes. 
-    
+
     """
     gui.moveTo(position)
     gui.click()
@@ -65,7 +64,7 @@ def retry_location_test(test_index, ui_test):
 
     Function retries image location. There could
     be cases where there is a slow internet connection
-    
+
     Assumption, user ran the temperature bar test
     so firefox and the navigator page are open.
     """
@@ -77,25 +76,38 @@ def retry_location_test(test_index, ui_test):
         test_index, confidence=0.3, grayscale=True)
 
     if image_loc is None:
-        gui.alert(text='Index not found!', title='{}'.format(ui_test), button='OK', timeout=box_timeout)
+        gui.alert(text='Index not found!', title='{}'.format(
+            ui_test), button='OK', timeout=box_timeout)
         result = 'Test Failed'
     else:
         time.sleep(0)
-        gui.alert(text='UI test complete!', title='{}'.format(ui_test), button='Close', timeout=box_timeout)
+        gui.alert(text='UI test complete!', title='{}'.format(
+            ui_test), button='Close', timeout=box_timeout)
         result = 'Test Completed'
 
     # Close index sub-tab
     time.sleep(.30)
-    #gui.click(dimension['close_index'])
+    # gui.click(dimension['close_index'])
     time.sleep(.30)
     return result
+
+
+def clear_cache():
+    """
+        Function to clear Firefox cache
+    """
+    gui.alert('Clearing browser cache...', 'Cache', timeout=box_timeout)
+    time.sleep(2)
+    gui.hotkey('ctrl', 'shift', 'r')
+    time.sleep(2)
+
 
 def test_profile(test_index, ui_test, location, log_result, log_time):
     """
 
     Function retries image location. There could
     be cases where there is a slow internet connection
-    
+
     Assumption, user ran the temperature bar test
     so firefox and the navigator page are open.
     """
@@ -110,13 +122,15 @@ def test_profile(test_index, ui_test, location, log_result, log_time):
         test_index, confidence=0.3, grayscale=True)
 
     if image_loc is None:
-        gui.alert(text='Index failed time test!', title='{}'.format(ui_test), button='OK', timeout=box_timeout)
+        gui.alert(text='Index failed time test!', title='{}'.format(
+            ui_test), button='OK', timeout=box_timeout)
         result = 'Index failed time(10 seconds) test'
     else:
         time.sleep(0)
-        gui.alert(text='UI test complete!', title='{}'.format(ui_test), button='Close', timeout=box_timeout)
+        gui.alert(text='UI test complete!', title='{}'.format(
+            ui_test), button='Close', timeout=box_timeout)
         result = 'Test Completed'
-    
+
     # Log resultd
     test_date = datetime.now()
     test_date = test_date.strftime("%A, %d. %B %Y %H:%M:%S")
@@ -124,13 +138,16 @@ def test_profile(test_index, ui_test, location, log_result, log_time):
     log_time[ui_test] = test_date
     return result
 
+
 def get_time():
     test_date = datetime.now()
     test_date = test_date.strftime("%A, %d. %B %Y %H:%M:%S")
     return test_date
 
+
 def main():
     navigator_webpage()
+
 
 if __name__ == '__main__':
     main()
