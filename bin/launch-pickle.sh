13371e86110389a329ece21d0e7e37b4b42e6fb9
Dwayne Hart <44408505+dwayne-hart@users.noreply.github.com>
2020-07-21 13:48:46 -0230

    Add helper script to run python tests

--- bin/launch-pickle.sh
+++ bin/launch-pickle.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-python ${HOME}/Ocean-Data-Map-Project/scripts/generate_class4_list.py --config ${HOME}/Ocean-Data-Map-Project/oceannavigator/oceannavigator.cfg
+. /opt/config/etc/profile.d/ocean-navigator.sh
+
+python /opt/Ocean-Data-Map-Project/scripts/generate_class4_list.py --config /op}t/Ocean-Data-Map-Project/oceannavigator/oceannavigator.cfg
