89a42d39ad43098bf9bb8ee54423b751459d6873
Dwayne Hart <dwayne.hart@gmail.com>
2020-08-10 13:43:26 -0230

    Updated the script to launch two gunicorn processes per cpu processor found.

--- bin/runserver.sh
+++ bin/runserver.sh
@@ -6,6 +6,10 @@
 
 [[ x"$1" == x"" ]] && PORT=5000 || PORT=$1
 
-NUMBER_PROCS=$(awk /processor/'{processor++} END {print processor}' < /proc/cpuinfo)
+if [ ! -e /usr/bin/bc ] ; then
+    NUMBER_PROCS=$(awk /^processor/'{processor++} END {print processor}' < /proc/cpuinfo)
+else
+    NUMBER_PROCS=$(echo "$(awk /^processor/'{processor++} END {print processor}' < /proc/cpuinfo) * 2" | bc)
+fi
 
 gunicorn -w $((NUMBER_PROCS)) -t 300 --graceful-timeout 300 --preload -b 0.0.0.0:$((PORT)) --reload "oceannavigator:create_app()" $2
