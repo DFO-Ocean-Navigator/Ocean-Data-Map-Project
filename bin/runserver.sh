795cbb54adaa79b02327dc8231257a035043adeb
Dwayne Hart <dwayne.hart@gmail.com>
2020-08-24 10:21:01 -0230

    Changed the [...]/bin/runserver.sh script.
        If the bc binary is not installed just go with the number of processing units the machine has either by looking at the /proc/cpuinfo or using the nproc
        command to determine the number of processing units available.
    
        Using one of physical, fully virtual, LXD, and Docker frameworks changes the information contained in the /proc/cpuinfo file. To get around this the
        easiet solution is to use a built in command.
    
    Changed the [...]/bin/launch-pickle.sh script.
    
        Depending on the version of the Ocean-Data-Map-Project certain shell environment variables are created either from /opt/.... or in a user's home directory.

--- bin/runserver.sh
+++ bin/runserver.sh
@@ -6,10 +6,25 @@
 
 [[ x"$1" == x"" ]] && PORT=5000 || PORT=$1
 
-if [ ! -e /usr/bin/bc ] ; then
-    NUMBER_PROCS=$(awk /^processor/'{processor++} END {print processor}' < /proc/cpuinfo)
-else
-    NUMBER_PROCS=$(echo "$(awk /^processor/'{processor++} END {print processor}' < /proc/cpuinfo) * 2" | bc)
+if [ ! -e /usr/bin/nproc ] ; then
+
+    if [ ! -e /usr/bin/bc ] ; then
+        NUMBER_WORKERS=$(awk /^processor/'{processor++} END {print processor}' < /proc/cpuinfo)
+        WORKER_THREADS=${NUMBER_WORKERS}
+    else
+        NUMBER_WORKERS=$(echo "($(awk /^processor/'{processor++} END {print processor}' < /proc/cpuinfo) * 2)+1" | bc)
+    fi
+
+else 
+
+    if [ ! -e /usr/bin/bc ] ; then
+        NUMBER_WORKERS=$(nproc)
+        WORKER_THREADS=${NUMBER_WORKERS}
+    else
+        NUMBER_WORKERS=$(echo "($(nproc) * 2)+1" | bc)
+        WORKER_THREADS=$(echo "(${NUMBER_WORKERS} *2 )" | bc)
+    fi
+
 fi
 
-gunicorn -w $((NUMBER_PROCS)) -t 300 --graceful-timeout 300 --preload -b 0.0.0.0:$((PORT)) --reload "oceannavigator:create_app()" $2
+gunicorn -w $((NUMBER_WORKERS)) --threads $((WORKER_THREADS)) --worker-class=gthread --worker-tmp-dir /dev/shm -t 300 --graceful-timeout 300 --preload -b 0.0.0.0:$((PORT)) --reload "oceannavigator:create_app()" $2
