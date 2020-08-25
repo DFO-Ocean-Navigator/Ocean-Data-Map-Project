18cadc12a7b86ea4d42bf99abc39b313243690af
Dwayne Hart <44408505+dwayne-hart@users.noreply.github.com>
2020-08-24 22:18:30 -0230

    Update to two scripts for determining the number of workers & threads. The other for creating a pickle file. (#752)
    * Changed the [...]/bin/runserver.sh script.
    
        If the bc binary is not installed just go with the number of processing units the machine has either by looking at the /proc/cpuinfo or using the nproc
        command to determine the number of processing units available.
    
        Using one of physical, fully virtual, LXD, and Docker frameworks changes the information contained in the /proc/cpuinfo file. To get around this the
        easiet solution is to use a built in command.
    
    Changed the [...]/bin/launch-pickle.sh script.
    
        Depending on the version of the Ocean-Data-Map-Project certain shell environment variables are created either from /opt/.... or in a user's home directory.
    
    * Added in some additional fixes.

--- bin/launch-pickle.sh
+++ bin/launch-pickle.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-. /opt/config/etc/profile.d/ocean-navigator.sh
+if [ -e /opt/config/etc/profile.d/ocean-navigator.sh ] ; then 
 
-python /opt/Ocean-Data-Map-Project/scripts/generate_class4_list.py --config /op}t/Ocean-Data-Map-Project/oceannavigator/oceannavigator.cfg
+    . /opt/config/etc/profile.d/ocean-navigator.sh
+    python /opt/Ocean-Data-Map-Project/scripts/generate_class4_list.py --config /opt/Ocean-Data-Map-Project/oceannavigator/oceannavigator.cfg
+
+else
+
+    . ${HOME}/.bashrc
+    python ${HOME}/Ocean-Data-Map-Project/scripts/generate_class4_list.py --config ${HOME}/Ocean-Data-Map-Project/oceannavigator/oceannavigator.cfg
+
+fi
