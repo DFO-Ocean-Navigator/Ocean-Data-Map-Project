355152670ea8cde25abe5bf6e35c55f204ac71b5
Doug Latornell <doug.latornell@43ravens.ca>
2021-01-15 12:45:39 -0800

    Update class4 index pickle generator script (#809)
    * Update generate_class4_list script
    
    * Use pathlib.Path because Python>=3.6
    * Use set and list comprehensions because modern Python
    * Sort the class4 files dict in reverse date order because MapToolbar.jsx uses
    the first element to set the month displayed and we want that to be the most
    recently indexed class4 files.
    * Rename data.class4.__list_class4_files_slowly() to
      data.class4._list_class4_files_slowly() because leading-single-underscore
      "internal use" convention is sufficient; leading-double-underscore name
      mangling just makes life difficult for IDE users
    * Use generate_class4_list.list_class4_files() in
      data.class4._list_class4_files_slowly() to eliminate duplicated code
    
    * Drop unnecessary `#!/usr/bin/env python`
    
    * Ensure init of class4 pickle lock mgmt vars
    
    * Drop bare Exception re: pickle.dump()
    
    It's unclear what error condition this is supposed to catch and bare Exceptions
    are bad form. We can add specific exception handling back if/when we figure out
    what this try/except was protecting against.
    
    * Change to use f-strings
    
    * Update module docstring

--- scripts/generate_class4_list.py
+++ scripts/generate_class4_list.py
@@ -1,29 +1,19 @@
-#!/usr/bin/env python
-
 """
-Generate a list of Class 4 files and save it to a cache file.
+Generate an index of Class 4 files and save it as a Python pickle cache file.
 
-Author: Clyde Clements
-Created: 2017-10-02
-
-This script generates the list of Class 4 files by crawling the THREDDS URL of
-the associated catalog. It is intended to be run from a cron job and its output
-cache file used from within the Ocean Navigator code.
+This script generates an index of Class 4 files by globbing the Class 4 storage tree
+in search of GIOPS Class 4 profile files. It is intended to be run from a cron job
+and its output cache file used from within the Ocean Navigator code.
 """
 
 import argparse
 import datetime
 import fcntl
-import glob
 import logging
-import os.path
+from pathlib import Path
+import pickle
 import sys
 import time
-
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
 
 
 logging.basicConfig(format='%(message)s', level=logging.INFO)
@@ -31,22 +21,14 @@ log = logging.getLogger()
 
 
 def list_class4_files(class4_path):
-    files = []
-    result = []
-    
-    for f in glob.iglob(f"{class4_path}/**/*.nc", recursive=True):
-        if f.endswith('profile.nc') and ('GIOPS' in f):
-            files.append(os.path.splitext(os.path.basename(f))[0])
-
-
-    for names in files:
-        value = names
-        date = datetime.datetime.strptime(value.split("_")[1], "%Y%m%d")
-        result.append({
-            'name': date.strftime("%Y-%m-%d"),
-            'id': value
-        })
-
+    files = {f for f in Path(class4_path).glob("**/*GIOPS*profile.nc")}
+    result = [
+        {
+            "name": datetime.datetime.strptime(class4_id.split("_")[1], "%Y%m%d").strftime("%Y-%m-%d"),
+            "id": class4_id
+        }
+        for class4_id in sorted((f.stem for f in files), reverse=True)
+    ]
     return result
 
 
@@ -60,8 +42,7 @@ def main():
     config = {}
     log.info(f"Attempting to load Ocean Navigator configuration from file {opts.config_file.name}...")
     try:
-        exec(compile(opts.config_file.read(), opts.config_file.name, 'exec'),
-             config)
+        exec(compile(opts.config_file.read(), opts.config_file.name, 'exec'), config)
     except IOError:
         log.error(f"Error: Unable to load configuration file {opts.config_file.name}.")
         sys.exit(1)
@@ -77,19 +58,17 @@ def main():
     log.info(f"Generating list of Class4 files from {config['CLASS4_PATH']}...")
     class4_files = list_class4_files(config['CLASS4_PATH'])
 
-    output_file_name = os.path.join(config['CACHE_DIR'], 'class4_files.pickle')
+    output_file_name = Path(config['CACHE_DIR'], 'class4_files.pickle')
     try:
         output_file = open(output_file_name, 'wb')
     except IOError:
-        log.error('Unable to open output file %s', output_file_name)
+        log.error(f"Unable to open output file {output_file_name}")
         sys.exit(1)
 
-    log.info('Obtaining exclusive lock on output file %s ...',
-             output_file_name)
+    log.info(f"Obtaining exclusive lock on output file {output_file_name }...")
     # Make at most "max_tries" attempts to acquire the lock.
-    max_tries = 10
-    num_tries = 0
-    attempt_lock = True
+    num_tries, max_tries = 0, 10
+    attempt_lock, lock_acquired = True, False
     while attempt_lock:
         try:
             fcntl.lockf(output_file, fcntl.LOCK_EX)
@@ -105,21 +84,17 @@ def main():
             attempt_lock = False
 
     if not lock_acquired:
-        log.error('Unable to acquire lock on output file %s', output_file_name)
+        log.error(f"Unable to acquire lock on output file {output_file_name}")
         sys.exit(1)
 
     log.info('Writing list of Class4 files to output file ...')
-    try:
-        pickle.dump(class4_files, output_file)
-    except Exception:
-        log.error('Unable to dump to output file %s', output_file_name)
-        sys.exit(1)
+    pickle.dump(class4_files, output_file)
 
     log.info('Releasing lock on output file ...')
     try:
         fcntl.lockf(output_file, fcntl.LOCK_UN)
     except IOError:
-        log.error('Unable to release lock on output file %s', output_file_name)
+        log.error(f"Unable to release lock on output file {output_file_name}")
         sys.exit(1)
     finally:
         output_file.close()
