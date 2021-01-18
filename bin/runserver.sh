58d075685b7197f4a2abac30fa676d3025c99cdb
Dwayne Hart <44408505+dwayne-hart@users.noreply.github.com>
2021-01-18 11:51:29 -0330

    Update runserver.sh
    Statically set the value to the WORKER_THREADS variable to 5. Otherwise if we allow the system to use the rule of thumb (2*NUMBER_WORKERS) to set the number of WORKER_THREADS we will run out of memory resources within the LXD/LXC containers.
    
    Also, to cut down on the virutal RAM usage the "--worker-tmp-dir /dev/shm" option for gunicorn was removed. The default behavior is for gunicorn to use the /tmp directory for writing temporary files.

--- bin/runserver.sh
+++ bin/runserver.sh
@@ -3,6 +3,7 @@
 # Usage: ./runserver.sh <port> <optional_dataset_config_file.json>
 # <optional_dataset_config_file.json>:  Specify a non-default dataset config file to load the Navigator with.
 #                                       Argument not required.
+WORKER_THREADS=5
 
 [[ x"$1" == x"" ]] && PORT=5000 || PORT=$1
 
@@ -10,7 +11,6 @@ if [ ! -e /usr/bin/nproc ] ; then
 
     if [ ! -e /usr/bin/bc ] ; then
         NUMBER_WORKERS=$(awk /^processor/'{processor++} END {print processor}' < /proc/cpuinfo)
-        WORKER_THREADS=${NUMBER_WORKERS}
     else
         NUMBER_WORKERS=$(echo "($(awk /^processor/'{processor++} END {print processor}' < /proc/cpuinfo) * 2)+1" | bc)
     fi
@@ -19,12 +19,10 @@ else
 
     if [ ! -e /usr/bin/bc ] ; then
         NUMBER_WORKERS=$(nproc)
-        WORKER_THREADS=${NUMBER_WORKERS}
     else
         NUMBER_WORKERS=$(echo "($(nproc) * 2)+1" | bc)
-        WORKER_THREADS=$(echo "(${NUMBER_WORKERS} *2 )" | bc)
     fi
 
 fi
 
-gunicorn -w $((NUMBER_WORKERS)) --threads $((WORKER_THREADS)) --worker-class=gthread --worker-tmp-dir /dev/shm -t 300 --graceful-timeout 300 --preload -b 0.0.0.0:$((PORT)) --reload "oceannavigator:create_app()" $2
+gunicorn -w $((NUMBER_WORKERS)) --threads $((WORKER_THREADS)) --worker-class=gthread -t 300 --graceful-timeout 300 --preload -b 0.0.0.0:$((PORT)) --reload "oceannavigator:create_app()" $2
