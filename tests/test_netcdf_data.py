002c8239871225ed80ecc71ad94d639d37ac1391
Doug Latornell <doug.latornell@43ravens.ca>
2021-03-12 14:18:53 -0800

    Get model class from config instead of "sniffing" dataset dimensions (#814)
    * Get model class from config instead of dims sniffing
    
    re: issue #664
    
    * Get dimensions from dataset object
    
    No need now to access database or open dataset file because we are no longer
    looking at the dimensions to determine what kind of model object to create.
    
    re: issue #664 and pull request #811
    
    * Drop meta_only arg/attr
    
    It's no longer required when the model data class is supplied in the config.
    
    * Whitespace cleanup
    
    * Delete unused imports
    
    * Delete another unused import
    
    Flagged by LGTM in PR checks.
    
    * Fix issues flagged by CodeFactor in PR checks
    
    * Delete unused variables
    * Delete commented out code
    * Change to dict literal
    * Simplify if-else block
    * Correct comparison to None
    * Delete unnecessary else

--- tests/test_netcdf_data.py
+++ tests/test_netcdf_data.py
@@ -54,21 +54,14 @@ class TestNetCDFData(unittest.TestCase):
         self.assertEqual(nc_data.interp, "gaussian")
         self.assertEqual(nc_data.radius, 25000)
         self.assertEqual(nc_data.neighbours, 10)
-        self.assertFalse(nc_data.meta_only)
         self.assertIsNone(nc_data.dataset)
         self.assertIsNone(nc_data._variable_list)
-        self.assertEqual(nc_data._nc_files, [])
         self.assertEqual(nc_data._grid_angle_file_url, "")
         self.assertIsNone(nc_data._time_variable)
         self.assertFalse(nc_data._dataset_open)
         self.assertEqual(nc_data._dataset_key, "")
         self.assertIsNone(nc_data._dataset_config)
-
-    def test_enter_meta_only(self):
-        kwargs = {"meta_only": True}
-        with NetCDFData("tests/testdata/nemo_test.nc", **kwargs) as nc_data:
-            self.assertFalse(nc_data._dataset_open)
-            self.assertIsNone(nc_data.dataset)
+        self.assertIsNone(nc_data._nc_files)
 
     def test_enter_nc_files_list(self):
         nc_data = NetCDFData("tests/testdata/nemo_test.nc")
@@ -295,8 +288,8 @@ class TestNetCDFData(unittest.TestCase):
             self.assertEqual(variables[0].valid_max, 373.0)
 
     def test_xarray_dimensions(self):
-        nc_data = NetCDFData("tests/testdata/mercator_test.nc")
-        self.assertEqual(['time', 'depth', 'latitude', 'longitude'], nc_data.dimensions)
+        with NetCDFData("tests/testdata/mercator_test.nc") as nc_data:
+            self.assertEqual(['depth', 'latitude', 'longitude', 'time'], nc_data.dimensions)
 
     def test_zarr_xarray_variables(self):
         with NetCDFData("tests/testdata/giops_test.zarr") as nc_data:
@@ -312,8 +305,8 @@ class TestNetCDFData(unittest.TestCase):
             self.assertEqual(variables[0].valid_max, 373.0)
 
     def test_zarr_xarray_dimensions(self):
-        nc_data = NetCDFData("tests/testdata/giops_test.zarr")
-        self.assertEqual(['depth', 'latitude', 'longitude', 'time'], nc_data.dimensions)
+        with NetCDFData("tests/testdata/giops_test.zarr") as nc_data:
+            self.assertEqual(['depth', 'latitude', 'longitude', 'time'], nc_data.dimensions)
 
     def test_fvcom_variables(self):
         with NetCDFData("tests/testdata/fvcom_test.nc") as nc_data:
@@ -329,24 +322,8 @@ class TestNetCDFData(unittest.TestCase):
             self.assertIsNone(variables[3].valid_max)
 
     def test_fvcom_dimensions(self):
-        nc_data = NetCDFData("tests/testdata/fvcom_test.nc")
-        self.assertEqual(["time", "maxStrlen64", "node", "siglay"], nc_data.dimensions)
-
-    def test_fvcom_dimensions_file_not_found(self):
-        nc_data = NetCDFData("tests/testdata/not_fvcom_test.nc")
-        self.assertEqual([], nc_data.dimensions)
-
-    def test_sqlite3_dimensions(self):
-        nc_data = NetCDFData("tests/testdata/databases/Historical.sqlite3")
-        self.assertEqual(['y', 'x', 'time_counter', 'axis_nbounds', 'depthv'], nc_data.dimensions)
-
-    def test_sqlite3_dimensions_operational_error(self):
-        nc_data = NetCDFData("tests/testdata/databases/not_Historical.sqlite3")
-        self.assertEqual([], nc_data.dimensions)
-
-    def test_dimensions_url_list(self):
-        nc_data = NetCDFData(["tests/testdata/nemo_test.nc", "tests/testdata/nemo_grid_angle.nc"])
-        self.assertEqual(['deptht', 'time_counter', 'x', 'y'], nc_data.dimensions)
+        with NetCDFData("tests/testdata/fvcom_test.nc") as nc_data:
+            self.assertEqual(["time", "maxStrlen64", "node", "siglay"], nc_data.dimensions)
 
     def test_variable_list_cached(self):
         with NetCDFData("tests/testdata/nemo_test.nc") as nc_data:
@@ -391,7 +368,7 @@ class TestNetCDFData(unittest.TestCase):
         kwargs = {"dataset_key": "giops"}
         with NetCDFData("tests/testdata/nemo_test.nc", **kwargs) as nc_data:
             file_list = nc_data.get_nc_file_list(nc_data._dataset_config)
-            self.assertEqual(nc_data._nc_files, [])
+            self.assertIsNone(nc_data._nc_files)
 
     @patch("data.netcdf_data.DatasetConfig._get_dataset_config")
     def test_get_nc_file_list_no_dataset_config_url(self, patch_get_dataset_config):
@@ -400,24 +377,7 @@ class TestNetCDFData(unittest.TestCase):
         kwargs = {"dataset_key": "giops_no_url"}
         with NetCDFData("tests/testdata/nemo_test.nc", **kwargs) as nc_data:
             nc_data.get_nc_file_list(nc_data._dataset_config)
-            self.assertEqual(nc_data._nc_files, [])
-
-    @patch("data.netcdf_data.DatasetConfig._get_dataset_config")
-    def test_get_nc_file_no_variable_kwarg_raises(self, patch_get_dataset_config):
-        patch_get_dataset_config.return_value = self.patch_dataset_config_ret_val
-
-        with NetCDFData("tests/testdata/nemo_test.nc", **{"dataset_key": "nemo_sqlite3"}) as nc_data:
-            with self.assertRaises(RuntimeError):
-                nc_data.get_nc_file_list(nc_data._dataset_config)
-
-    @patch("data.netcdf_data.DatasetConfig._get_dataset_config")
-    def test_get_nc_file_no_timestep_kwarg_raises(self, patch_get_dataset_config):
-        patch_get_dataset_config.return_value = self.patch_dataset_config_ret_val
-
-        with NetCDFData("tests/testdata/nemo_test.nc", **{"dataset_key": "nemo_sqlite3"}) as nc_data:
-            with self.assertRaises(RuntimeError):
-                kwargs = {"variable": "votemper"}
-                nc_data.get_nc_file_list(nc_data._dataset_config, **kwargs)
+            self.assertIsNone(nc_data._nc_files)
 
     def test_get_dataset_variable_raises_on_unknown_variable(self):
         with NetCDFData("tests/testdata/nemo_test.nc") as nc_data:
