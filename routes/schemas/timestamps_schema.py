7b358de2494bc15b51bb28a3cf987b02fc491362
htmlboss <nmiri127@gmail.com>
2021-07-08 02:40:36 +0000

    Exclude unknown fields in TimestampsSchema
    This is a workaround for jQuery sending a hidden _=1625711072192 parameter as part of the timestamps payload....not sure why it's doing that. Anyway there's no further security risk as we just silently ignore this unknown value

--- routes/schemas/timestamps_schema.py
+++ routes/schemas/timestamps_schema.py
@@ -1,4 +1,5 @@
-from marshmallow import Schema, fields
+from marshmallow import EXCLUDE, Schema, fields
+
 
 class TimestampsSchema(Schema):
     """
@@ -8,6 +9,9 @@ class TimestampsSchema(Schema):
     * dataset: dataset key (e.g. giops_day)
     * variable: variable key (e.g. votemper)
     """
+
+    class Meta:
+        unknown = EXCLUDE # workaround for hidden "_" field being sent by jQuery
 
     dataset = fields.Str(required=True)
     variable = fields.Str(required=True)
