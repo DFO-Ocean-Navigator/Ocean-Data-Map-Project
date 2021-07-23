1cec0064e60d7d378ae1d4075eae5479ec0cf29a
htmlboss <nmiri127@gmail.com>
2021-07-23 03:12:07 +0000

    Update linting rules

--- oceannavigator/frontend/.eslintrc.js
+++ oceannavigator/frontend/.eslintrc.js
@@ -1,73 +1,29 @@
 module.exports = {
-   
-  "env": {
-    "browser": true,
-    "es6": true,
-    "node" : true,
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true,
   },
-  "extends": ["eslint:recommended", "plugin:react/recommended"],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true,
-      "templateStrings": true
+  'settings': {
+    'react': {
+      'version': 'detect',
     },
-    "sourceType": "module"
   },
-  "plugins": [
-    "react"
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'google',
   ],
-  "rules": {
-    "indent": [
-      "error",
-      2,
-      {
-        "SwitchCase": 1
-      }
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "double"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ],
-    "curly": [
-      "error",
-      "all"
-    ],
-    "no-console": [
-      "error",
-      {
-        "allow": ["warn", "error"]
-      }
-    ],
-    "no-unused-vars": [
-      "error",
-      {
-        "varsIgnorePattern": "i18n",
-        "args": "none"
-      }
-    ],
-    "max-len": [
-      "error",
-      {
-        "code": 80,
-        "ignoreStrings": true
-      }
-    ],
-    "react/jsx-uses-vars": "error",
-    "react/jsx-uses-react": "error",
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module',
   },
-  "globals": {
-    "$": true,
-    "jQuery": true,
-    "_": true,
-    "require": true,
-  }
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+  },
 };
