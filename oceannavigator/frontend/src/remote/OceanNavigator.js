da0f0e94428f1f0fbdf0708db0994f1f55ff015f
htmlboss <nmiri127@gmail.com>
2021-07-25 15:18:41 +0000

    Use axios for fetching timestamps in OceanNavigator component

--- oceannavigator/frontend/src/remote/OceanNavigator.js
+++ oceannavigator/frontend/src/remote/OceanNavigator.js
@@ -1,24 +1,36 @@
 // Cannot use async/awat syntax here since we still have clients
 // that use IE which doesn't support that.
 
-const axios = require('axios');
+const axios = require("axios");
 
 function _createPromise() {
-    return axios.create();
+  return axios.create();
 }
 
 
 export function GetDatasetsPromise() {
-    return _createPromise().get("/api/v1.0/datasets");
+  return _createPromise().get("/api/v1.0/datasets");
 }
 
 export function GetVariablesPromise(dataset) {
-    return _createPromise().get(
-        "/api/v1.0/variables",
-        {
-            params: {
-                dataset: dataset
-            }
-        }
-    );
+  return _createPromise().get(
+    "/api/v1.0/variables",
+    {
+      params: {
+        dataset: dataset
+      }
+    }
+  );
+}
+
+export function GetTimestampsPromise(dataset, variable) {
+  return _createPromise().get(
+    "/api/v1.0/timestamps",
+    {
+      params: {
+        dataset: dataset,
+        variable: variable
+      }
+    }
+  );
 }
