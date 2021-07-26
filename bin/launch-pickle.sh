aaee6b27b096d99798a84449fd1c4c1d6216c542
Dwayne Hart <44408505+dwayne-hart@users.noreply.github.com>
2021-07-26 11:52:08 -0230

    Removing older conditional logic. (#870)

--- bin/launch-pickle.sh
+++ bin/launch-pickle.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -e /opt/config/etc/profile.d/ocean-navigator.sh ] ; then 
-
-    . /opt/config/etc/profile.d/ocean-navigator.sh
-    python /opt/Ocean-Data-Map-Project/scripts/generate_class4_list.py --config /opt/Ocean-Data-Map-Project/oceannavigator/oceannavigator.cfg
-
-else
-
-    . ${HOME}/.bashrc
-    python ${HOME}/Ocean-Data-Map-Project/scripts/generate_class4_list.py --config ${HOME}/Ocean-Data-Map-Project/oceannavigator/oceannavigator.cfg
-
-fi
+source ${HOME}/tools/conf/ocean-navigator-env.sh
+conda activate navigator
+python ${HOME}/Ocean-Data-Map-Project/scripts/generate_class4_list.py --config ${HOME}/Ocean-Data-Map-Project/oceannavigator/oceannavigator.cfg
