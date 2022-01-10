9367b869f516d8a18016d6b0c14d53277813da79
htmlboss <nmiri127@gmail.com>
2022-01-10 23:14:54 +0000

    Revive dataset help button

--- oceannavigator/frontend/src/components/lib/SelectBox.jsx
+++ oceannavigator/frontend/src/components/lib/SelectBox.jsx
@@ -1,12 +1,33 @@
 import React from "react";
-import { FormGroup, ControlLabel, FormControl } from "react-bootstrap";
+import { FormGroup, ControlLabel, FormControl, Modal, Button } from "react-bootstrap";
 import PropTypes from "prop-types";
+
+import Icon from "./Icon";
+
+import { withTranslation } from "react-i18next";
 
 const fastEqual = require("fast-deep-equal/es6/react");
 
-export default class SelectBox extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    return !fastEqual(this.props, nextProps);
+export class SelectBox extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showHelp: false,
+    };
+
+    this.toggleShowHelp = this.toggleShowHelp.bind(this);
+  }
+
+  toggleShowHelp() {
+    this.setState(prevState => ({
+      showHelp: !prevState.showHelp,
+    }));
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return !fastEqual(this.props, nextProps) ||
+      !fastEqual(this.state, nextState);
   }
 
   render() {
@@ -28,27 +49,59 @@ export default class SelectBox extends React.Component {
                      !this.props.options.length;
 
     return (
-      <FormGroup controlid={`formgroup-${this.props.id}-selectbox`}>
-        <ControlLabel>{this.props.label}</ControlLabel>
-        <FormControl
-          componentClass="select"
-          name={this.props.name}
-          placeholder={disabled ? _("Loading...") : this.props.placeholder}
-          onChange={(e) => {
-            if (this.props.multiple) {
-              this.props.onChange(e.target.name, e.target.selectedOptions);
-            }
-            else {
-              this.props.onChange(e.target.name, e.target.value);
-            }
-          }}
-          disabled={disabled}
-          value={this.props.selected}
-          multiple={this.props.multiple}
+      <>
+        <FormGroup controlid={`formgroup-${this.props.id}-selectbox`}>
+          <ControlLabel>{this.props.label}</ControlLabel>
+          
+          <Button
+            onClick={this.toggleShowHelp}
+            bsStyle="default"
+            bsSize="xsmall"
+            style={{"display": this.props.helpContent ? "block" : "none", "float": "right"}}
+          >
+            ?
+          </Button>
+
+          <FormControl
+            componentClass="select"
+            name={this.props.name}
+            placeholder={disabled ? _("Loading...") : this.props.placeholder}
+            onChange={(e) => {
+              if (this.props.multiple) {
+                this.props.onChange(e.target.name, e.target.selectedOptions);
+              }
+              else {
+                this.props.onChange(e.target.name, e.target.value);
+              }
+            }}
+            disabled={disabled}
+            value={this.props.selected}
+            multiple={this.props.multiple}
+          >
+            {options}
+          </FormControl>
+        </FormGroup>
+
+        <Modal
+          show={this.state.showHelp}
+          onHide={this.toggleShowHelp}
+          bsSize="large"
+          dialogClassName="helpdialog"
+          backdrop={true}
         >
-          {options}
-        </FormControl>
-      </FormGroup>
+          <Modal.Header closeButton closeLabel={_("Close")}>
+            <Modal.Title>{_("Help")}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {this.props.helpContent}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.toggleShowHelp}>
+              <Icon icon="close"/> {_("Close")}
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
     );
   }
 }
@@ -67,8 +120,12 @@ SelectBox.propTypes = {
     PropTypes.arrayOf(PropTypes.string),
   ]).isRequired,
   multiple: PropTypes.bool,
+  helpContent: PropTypes.arrayOf(PropTypes.object),
 };
 
 SelectBox.defaultProps = {
   multiple: false,
+  helpContent: null,
 };
+
+export default withTranslation()(SelectBox);
