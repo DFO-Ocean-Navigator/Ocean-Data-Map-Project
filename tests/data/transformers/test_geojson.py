2db7cffd11e1faa5fb47d1a699b3f61650a131f0
JustinElms <justin.elms@dfo-mpo.gc.ca>
2022-11-21 18:47:13 +0000

    fix geojson tests

--- tests/data/transformers/test_geojson.py
+++ tests/data/transformers/test_geojson.py
@@ -1,14 +1,10 @@
-"""Unit tests for data.geojson
-"""
-import unittest
-
 import xarray as xr
 from geojson import FeatureCollection
 
 from data.transformers.geojson import data_array_to_geojson
 
 
-class GeoJSONTest(unittest.TestCase):
+class GeoJSONTest:
     def setUp(self) -> None:
         self.data_array: xr.Dataset = xr.open_dataset(
             "tests/testdata/giops_test.nc", decode_times=False
@@ -17,8 +13,10 @@ class GeoJSONTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.data_array.close()
 
-    def test_data_array_to_geojson_builds_correct_feature_collection(self) -> None:
-        result = data_array_to_geojson(
+    async def test_data_array_to_geojson_builds_correct_feature_collection(
+        self,
+    ) -> None:
+        result = await data_array_to_geojson(
             self.data_array.votemper[0, 0, :5, :5],
             None,
             self.data_array["latitude"][:5],
@@ -28,9 +26,9 @@ class GeoJSONTest(unittest.TestCase):
         self.assertIsInstance(result, FeatureCollection)
         self.assertEqual(25, len(result["features"]))
 
-    def test_data_array_to_geojson_raises_when_data_not_2d(self) -> None:
+    async def test_data_array_to_geojson_raises_when_data_not_2d(self) -> None:
         with self.assertRaises(ValueError):
-            data_array_to_geojson(
+            await data_array_to_geojson(
                 self.data_array.votemper[:, 0, :5, :5],
                 None,
                 self.data_array["latitude"][:5],
