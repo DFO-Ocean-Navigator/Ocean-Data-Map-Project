574b603b1b2bcbdfbb0c929a827f75e33ced006e
JustinElms <justinwelms@gmail.com>
2023-12-10 00:17:26 +0000

    fix basesettings import

--- oceannavigator/settings.py
+++ oceannavigator/settings.py
@@ -2,7 +2,7 @@ import os
 from functools import lru_cache
 from typing import List
 
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
