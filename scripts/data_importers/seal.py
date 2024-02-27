e001d219cadaa4772e69c8890fc8a584a3ba6069
Justin Elms <79917349+JustinElms@users.noreply.github.com>
2024-02-27 14:51:29 -0330

    Update seal.py

--- scripts/data_importers/seal.py
+++ scripts/data_importers/seal.py
@@ -63,7 +63,6 @@ def main(uri: str, filename: str):
         with xr.open_dataset(fname) as ds:
             ds["TIME"] = ds.JULD.to_index().to_datetimeindex()
             ds["TIME"] = ds.TIME.swap_dims({"TIME": "N_PROF"})
-            
             depth = abs(gsw.conversions.z_from_p(
                 ds.PRES_ADJUSTED,
                 np.tile(ds.LATITUDE, (ds.PRES.shape[1], 1)).transpose(),
