b0c5c586dad6ab8ec4be3212e230e28ae4599197
JustinElms <justinwelms@gmail.com>
2025-02-24 13:59:10 +0000

    creat annotation button

---
+++ oceannavigator/frontend/src/components/AnnotationButton.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button, OverlayTrigger, Tooltip } from "react-bootstrap";
+
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { withTranslation } from "react-i18next";
+
+function AnnotationButton(props) {
+  return (
+    <OverlayTrigger
+      key="annotation-overlay"
+      placement="bottom"
+      overlay={<Tooltip id={"annotation-tooltip"}>{__("Add Map Annotation")}</Tooltip>}
+    >
+      <Button onClick={()=>{props.action("annotation");}} className="annotation-button">
+        <FontAwesomeIcon icon={faPencil} />
+      </Button>
+    </OverlayTrigger>
+  );
+}
+
+export default withTranslation()(AnnotationButton);
