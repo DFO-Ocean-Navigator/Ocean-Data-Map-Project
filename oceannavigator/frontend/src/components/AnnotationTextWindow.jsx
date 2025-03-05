daf01b6b10aa74e0e7121c4221922bfc2f809240
JustinElms <justinwelms@gmail.com>
2025-03-05 15:10:52 +0000

    add css to annotationtextwindow

--- oceannavigator/frontend/src/components/AnnotationTextWindow.jsx
+++ oceannavigator/frontend/src/components/AnnotationTextWindow.jsx
@@ -7,37 +7,33 @@ function AnnotationTextWindow(props) {
   const [inputText, setInputText] = useState("");
 
   const onSubmit = () => {
-    props.mapRef.current.addAnnotationLabel(inputText)
+    props.mapRef.current.addAnnotationLabel(inputText);
     props.updateUI({ modalType: "", showModal: false });
   };
 
-  const onUndo = () =>{
-    props.mapRef.current.undoAnnotationLabel()
-  }
+  const onUndo = () => {
+    props.mapRef.current.undoAnnotationLabel();
+  };
 
   const onClear = () => {
-    props.mapRef.current.clearAnnotationLabels()
-  }
+    props.mapRef.current.clearAnnotationLabels();
+  };
 
   return (
-    <Form>
-      <Form.Group>
-        <Form.Label>Annotation Text</Form.Label>
-        <Form.Control
-          type="text"
-          onChange={(e) => setInputText(e.target.value)}
-        />
-        <Button onClick={onSubmit}>
-          Add
-        </Button>
-        <Button onClick={onUndo}>
-          Undo
-        </Button>
-        <Button onClick={onClear}>
-          Clear
-        </Button>                
-      </Form.Group>
-    </Form>
+    <div className="annotation-text-window">
+      <Form.Label>Annotation Text</Form.Label>
+
+      <Form.Control
+        type="text"
+        onChange={(e) => setInputText(e.target.value)}
+      />
+
+      <div className="controls">
+        <Button onClick={onSubmit}>Add</Button>
+        <Button onClick={onUndo}>Undo</Button>
+        <Button onClick={onClear}>Clear</Button>
+      </div>
+    </div>
   );
 }
 
