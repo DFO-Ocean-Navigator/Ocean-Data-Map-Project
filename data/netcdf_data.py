7fc53b402884390939d5a3c84641caeb4cf0a796
Justin Elms <79917349+JustinElms@users.noreply.github.com>
2025-06-20 16:27:12 -0230

    Update netcdf_data.py

--- data/netcdf_data.py
+++ data/netcdf_data.py
@@ -426,8 +426,6 @@ class NetCDFData(Data):
                 subset = subset.assign(
                     **{
                         variable: self.get_dataset_variable(variable).isel(
-                    **{
-                        variable: self.get_dataset_variable(variable).isel(
                             **{
                                 y_coord: slice(0, subset[y_coord].size),
                                 x_coord: slice(0, subset[x_coord].size),
