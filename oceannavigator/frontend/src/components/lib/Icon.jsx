8aca2d6f0f32cbb0a49daa4bbe9918c3684ff8ad
Shasan634 <hasanshakib634@gmail.com>
2025-08-20 12:29:34 +0000

    removed translation from icon

--- oceannavigator/frontend/src/components/lib/Icon.jsx
+++ oceannavigator/frontend/src/components/lib/Icon.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import FontAwesome from "react-fontawesome";
 import PropTypes from "prop-types";
-import { withTranslation } from "react-i18next";
 
-const Icon = ({ icon, alt = "", t: _ }) => (
+const Icon = ({ icon, alt = "" }) => (
   <span className="Icon" title={alt}>
     <FontAwesome name={icon} />
     {alt && <span className="alt"> {alt}</span>}
@@ -13,7 +12,6 @@ const Icon = ({ icon, alt = "", t: _ }) => (
 Icon.propTypes = {
   icon: PropTypes.string.isRequired,
   alt: PropTypes.string,
-  t: PropTypes.func.isRequired,
 };
 
-export default withTranslation()(Icon);
+export default Icon;
