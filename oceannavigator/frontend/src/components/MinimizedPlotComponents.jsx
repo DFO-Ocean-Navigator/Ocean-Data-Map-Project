948d9927bad9388f374a8fd75352897be1abcfd7
JustinElms <justinwelms@gmail.com>
2025-10-09 15:09:57 +0000

    build plot windows and bar from plot data objs

--- oceannavigator/frontend/src/components/MinimizedPlotComponents.jsx
+++ oceannavigator/frontend/src/components/MinimizedPlotComponents.jsx
@@ -1,62 +1,29 @@
-import React, { useState, forwardRef, useImperativeHandle } from "react";
+import React from "react";
 import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark, faExpand } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from "prop-types";
 
-const MinimizedPlotBar = forwardRef(({ activePlotsRef }, ref) => {
-  const [minimizedPlots, setMinimizedPlots] = useState([]);
+const MinimizedPlotBar = ({ plotData, action }) => {
 
-  useImperativeHandle(ref, () => ({
-    addPlot: (plot) => {
-      setMinimizedPlots((prev) => {
-        if (prev.some(p => p.id === plot.id)) {
-          return prev;
-        }
-        return [...prev, plot];
-      });
-    },
-    
-    removePlot: (plotId) => {
-      setMinimizedPlots((prev) => prev.filter((plot) => plot.id !== plotId));
-    },
-    
-    clearAll: () => {
-      setMinimizedPlots([]);
-    },
-    
-    checkAndRestoreIfExists: (plotId) => {
-      const exists = minimizedPlots.some(plot => plot.id === plotId);
-      if (exists) {
-        handleRestore(plotId);
-        return true;
-      }
-      return false;
-    }
-  }), [minimizedPlots]);
-
-  const handleRestore = (plotId) => {
-    const plotToRestore = minimizedPlots.find((plot) => plot.id === plotId);
-    if (plotToRestore) {
-      setMinimizedPlots((prev) => prev.filter((plot) => plot.id !== plotId));
-      activePlotsRef.current?.restorePlot(plotToRestore);
-    }
+  const handleRestore = (plot) => {
+    action("updatePlot", { ...plot, active: true });
   };
 
-  const handleClose = (plotId) => {
-    setMinimizedPlots((prev) => prev.filter((plot) => plot.id !== plotId));
+  const handleClose = (plot) => {
+    action("closePlot", plot);
   };
 
-  if (!minimizedPlots?.length) return null;
+  let minimizedPlots = plotData?.filter((plot) => !plot.active);
 
-  return (
+  return minimizedPlots.length > 0 ? (
     <div className="minimized-plots-container">
       {minimizedPlots.map((plot) => (
         <div key={plot.id} className="minimized-plot-bar">
           <div className="minimized-plot-content">
             <span
               className="minimized-plot-title"
-              onClick={() => handleRestore(plot.id)}
+              onClick={() => handleRestore(plot)}
               style={{ cursor: "pointer" }}
               title="Click to restore"
             >
@@ -66,7 +33,7 @@ const MinimizedPlotBar = forwardRef(({ activePlotsRef }, ref) => {
               <Button
                 variant="outline-primary"
                 size="sm"
-                onClick={() => handleRestore(plot.id)}
+                onClick={() => handleRestore(plot)}
                 title="Restore"
               >
                 <FontAwesomeIcon icon={faExpand} />
@@ -74,7 +41,7 @@ const MinimizedPlotBar = forwardRef(({ activePlotsRef }, ref) => {
               <Button
                 variant="outline-danger"
                 size="sm"
-                onClick={() => handleClose(plot.id)}
+                onClick={() => handleClose(plot)}
                 title="Close"
               >
                 <FontAwesomeIcon icon={faXmark} />
@@ -84,11 +51,11 @@ const MinimizedPlotBar = forwardRef(({ activePlotsRef }, ref) => {
         </div>
       ))}
     </div>
-  );
-});
+  ) : null;
+};
 
 MinimizedPlotBar.propTypes = {
-  activePlotsRef: PropTypes.object.isRequired,
+  plotData: PropTypes.array.isRequired,
 };
 
 export default MinimizedPlotBar;
--- oceannavigator/frontend/src/components/PlotComponents.jsx
+++ oceannavigator/frontend/src/components/PlotComponents.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, forwardRef, useImperativeHandle } from "react";
+import React from "react";
 import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark, faMinus } from "@fortawesome/free-solid-svg-icons";
@@ -8,113 +8,9 @@ import LineWindow from "./LineWindow.jsx";
 import AreaWindow from "./AreaWindow.jsx";
 import TrackWindow from "./TrackWindow.jsx";
 import Class4Window from "./Class4Window.jsx";
-import { formatLatLon } from "./OceanNavigator.jsx";
 
-// Generate plot title from plotData
-export function getPlotTitle(plotData) {
-  if (!plotData) return "Plot";
-  const { type, coordinates, id, plotName } = plotData;
-  
-  if (plotName) return plotName;
-  
-  switch (type) {
-    case "Point":
-      return coordinates?.length
-        ? `Point Plot - ${formatLatLon(coordinates[0][0], coordinates[0][1])}`
-        : "Point Plot";
-    case "LineString":
-      return `Line Plot - ${coordinates?.length || 0} points`;
-    case "Polygon":
-      return `Area Plot - ${coordinates?.length || 0} vertices`;
-    case "track":
-      return id ? `Track Plot - ${id}` : "Track Plot";
-    case "class4":
-      return id ? `Class 4 Analysis - ${id}` : "Class 4 Analysis";
-    default:
-      return `${type} Plot`;
-  }
-}
-
-// Create plot component
-const createPlotComponent = (plotData, plotId, props) => {
-  const {
-    mapRef,
-    dataset0,
-    dataset1,
-    mapSettings,
-    names,
-    updateDataset0,
-    updateDataset1,
-    subquery,
-    action,
-    compareDatasets,
-    setCompareDatasets,
-    class4Type,
-    swapViews,
-  } = props;
-
-  const { type } = plotData;
-  const commonProps = {
-    plotData,
-    dataset_0: dataset0,
-    dataset_1: dataset1,
-    mapSettings,
-    names,
-    updateDataset0,
-    updateDataset1,
-    init: subquery,
-    action,
-    dataset_compare: compareDatasets,
-    setCompareDatasets,
-    key: plotId,
-    swapViews,
-  };
-
-  switch (type) {
-    case "Point":
-      return <PointWindow {...commonProps} updateDataset={updateDataset0} />;
-    case "LineString":
-      const line_distance =
-        mapRef.current?.getLineDistance?.(plotData.coordinates) || 0;
-      return (
-        <LineWindow
-          {...commonProps}
-          line_distance={line_distance}
-          onUpdate={updateDataset0}
-          swapViews={swapViews}
-        />
-      );
-    case "Polygon":
-      return <AreaWindow {...commonProps} swapViews={swapViews} />;
-    case "track":
-      return (
-        <TrackWindow
-          {...commonProps}
-          dataset={dataset0}
-          track={plotData.coordinates}
-          onUpdate={updateDataset0}
-        />
-      );
-    case "class4":
-      return (
-        <Class4Window
-          dataset={dataset0.id}
-          plotData={plotData}
-          class4type={plotData.class4type || class4Type}
-          init={subquery}
-          action={action}
-          key={plotId}
-        />
-      );
-    default:
-      return <div key={plotId}>Unknown plot type: {type}</div>;
-  }
-};
-
-const ActivePlotsContainer = forwardRef(({
-  newPlotData,
-  onMinimizedPlotsChange,
-  mapRef,
+const ActivePlotsContainer = ({
+  plotData,
   dataset0,
   dataset1,
   mapSettings,
@@ -127,152 +23,138 @@ const ActivePlotsContainer = forwardRef(({
   setCompareDatasets,
   class4Type,
   swapViews,
-}, ref) => {
-  const [activePlots, setActivePlots] = useState([]);
-  const lastProcessedPlotRef = useRef(null);
+}) => {
 
-  useImperativeHandle(ref, () => ({
-    restorePlot: (restoredPlot) => {
-      if (activePlots.some(plot => plot.id === restoredPlot.id)) {
-        return;
-      }
+  const handleMinimize = (plot) => {
+    action("updatePlot", { ...plot, active: false });
+  };
 
-      setActivePlots((prev) => {
-        const newActivePlots = [...prev];
-        if (newActivePlots.length >= 2) {
-          const oldestPlot = newActivePlots.shift();
-          onMinimizedPlotsChange?.({ action: 'add', plot: oldestPlot });
-        }
-        return [...newActivePlots, restoredPlot];
-      });
-    },
-    
-    clearAllPlots: () => {
-      setActivePlots([]);
-      lastProcessedPlotRef.current = null;
-      onMinimizedPlotsChange?.({ action: 'clearAll' });
-    }
-  }), [activePlots, onMinimizedPlotsChange]);
+  const handleClose = (plot) => {
+    action("closePlot", plot);
+  };
 
-useEffect(() => {
-  if (newPlotData?.type && newPlotData?.id) {
-    const plotId = newPlotData.id;
-    
-        if (lastProcessedPlotRef.current === plotId) {
-              return;
-                  }
-
-    // restores plot if it exists in the minimized window
-    if (onMinimizedPlotsChange) {
-      const shouldRestore = onMinimizedPlotsChange({ 
-        action: 'checkExists', 
-        plotId 
-      });
-      
-      if (shouldRestore) {
-        lastProcessedPlotRef.current = plotId;
-        return;
-      }
-    }
-
-    lastProcessedPlotRef.current = plotId;
-    const plotTitle = getPlotTitle(newPlotData);
-
-    const newActivePlot = {
-      id: plotId,
-      plotData: newPlotData,
-      title: plotTitle,
-    };
-
-    setActivePlots((prev) => {
-      const newActivePlots = [...prev];
-      if (newActivePlots.length >= 2) {
-        const oldestPlot = newActivePlots.shift();
-        onMinimizedPlotsChange?.({ action: 'add', plot: oldestPlot });
-      }
-      return [...newActivePlots, newActivePlot];
-    });
-  }
-}, [newPlotData, activePlots, onMinimizedPlotsChange]);
-
-  const handleMinimize = (plotId) => {
-    const plotToMinimize = activePlots.find((plot) => plot.id === plotId);
-    if (plotToMinimize) {
-      onMinimizedPlotsChange?.({ action: 'add', plot: plotToMinimize });
-      setActivePlots((prev) => prev.filter((plot) => plot.id !== plotId));
+  const createPlotComponent = (plot) => {
+    switch (plot.type) {
+      case "Point":
+        return (
+          <PointWindow
+            dataset_0={dataset0}
+            plotData={plot}
+            mapSettings={mapSettings}
+            updateDataset={updateDataset0}
+            init={subquery}
+            action={action}
+          />
+        );
+      case "LineString":
+        return (
+          <LineWindow
+            dataset_0={dataset0}
+            dataset_1={dataset1}
+            plotData={plot}
+            mapSettings={mapSettings}
+            names={names}
+            onUpdate={updateDataset0}
+            updateDataset0={updateDataset0}
+            updateDataset1={updateDataset1}
+            init={subquery}
+            action={action}
+            dataset_compare={compareDatasets}
+            setCompareDatasets={setCompareDatasets}
+            swapViews={swapViews}
+          />
+        );
+      case "Polygon":
+        return (
+          <AreaWindow
+            dataset_0={dataset0}
+            dataset_1={dataset1}
+            plotData={plot}
+            mapSettings={mapSettings}
+            names={names}
+            updateDataset0={updateDataset0}
+            updateDataset1={updateDataset1}
+            init={subquery}
+            action={action}
+            dataset_compare={compareDatasets}
+            setCompareDatasets={setCompareDatasets}
+            swapViews={swapViews}
+          />
+        );
+      case "track":
+        return (
+          <TrackWindow
+            dataset={dataset0}
+            track={coordinates}
+            names={names}
+            onUpdate={updateDataset0}
+            init={subquery}
+            action={action}
+          />
+        );
+      case "class4":
+        return (
+          <Class4Window
+            dataset={dataset0.id}
+            plotData={plot}
+            class4type={plot.class4type || class4Type}
+            init={subquery}
+            action={action}
+            key={plot.id}
+          />
+        );
     }
   };
 
-  const handleClose = (plotId) => {
-    setActivePlots((prev) => prev.filter((plot) => plot.id !== plotId));
-    onMinimizedPlotsChange?.({ action: 'remove', plotId });
-  };
-
-  if (!activePlots?.length) return null;
-
-  const createComponentProps = {
-    mapRef,
-    dataset0,
-    dataset1,
-    mapSettings,
-    names,
-    updateDataset0,
-    updateDataset1,
-    subquery,
-    action,
-    compareDatasets,
-    setCompareDatasets,
-    class4Type,
-    swapViews,
-  };
-
-  return (
-    <div
-      className={`active-plots-container ${
-        activePlots.length === 2 ? "side-by-side" : "single-plot"
-      }`}
-    >
-      {activePlots.map((plot) => (
-        <div
-          key={plot.id}
-          className={`active-plot-window ${
-            activePlots.length === 2 ? "half-width" : "full-width"
-          }`}
-        >
-          <div className="plot-window-header">
-            <h5 className="plot-window-title">{plot.title}</h5>
-            <div className="plot-window-controls">
-              <Button
-                variant="outline-primary"
-                size="sm"
-                onClick={() => handleMinimize(plot.id)}
-                title="Minimize"
-              >
-                <FontAwesomeIcon icon={faMinus} />
-              </Button>
-              <Button
-                variant="outline-primary"
-                size="sm"
-                onClick={() => handleClose(plot.id)}
-                title="Close"
-              >
-                <FontAwesomeIcon icon={faXmark} />
-              </Button>
-            </div>
-          </div>
-          <div className="plot-window-content">
-            {createPlotComponent(plot.plotData, plot.id, createComponentProps)}
+  let activePlots = plotData.filter((plot) => plot.active);
+  let plotComponents = activePlots.map((plot) => {
+    let plotWindow = createPlotComponent(plot);
+    return (
+      <div
+        key={plot.id}
+        className={`active-plot-window ${
+          activePlots.length === 2 ? "half-width" : "full-width"
+        }`}
+      >
+        <div className="plot-window-header">
+          <h5 className="plot-window-title">{plot.title}</h5>
+          <div className="plot-window-controls">
+            <Button
+              variant="outline-primary"
+              size="sm"
+              onClick={() => handleMinimize(plot)}
+              title="Minimize"
+            >
+              <FontAwesomeIcon icon={faMinus} />
+            </Button>
+            <Button
+              variant="outline-primary"
+              size="sm"
+              onClick={() => handleClose(plot)}
+              title="Close"
+            >
+              <FontAwesomeIcon icon={faXmark} />
+            </Button>
           </div>
         </div>
-      ))}
+        <div className="plot-window-content">{plotWindow}</div>
+      </div>
+    );
+  });
+
+  return plotComponents.length > 0 ? (
+    <div
+      className={`active-plots-container ${
+        plotComponents.length === 2 ? "side-by-side" : "single-plot"
+      }`}
+    >
+      {plotComponents}
     </div>
-  );
-});
+  ) : null;
+};
 
 ActivePlotsContainer.propTypes = {
-  newPlotData: PropTypes.object,
-  onMinimizedPlotsChange: PropTypes.func.isRequired,
-  mapRef: PropTypes.object.isRequired,
+  plotData: PropTypes.array.isRequired,
   dataset0: PropTypes.object.isRequired,
   dataset1: PropTypes.object.isRequired,
   mapSettings: PropTypes.object.isRequired,
