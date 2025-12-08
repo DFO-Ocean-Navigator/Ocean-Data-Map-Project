7f16c66d44d8ef6d0a1ceb993c24b62c8298a6ce
JustinElms <justinwelms@gmail.com>
2025-12-08 19:14:30 +0000

    convert trackwindow to functional component

--- oceannavigator/frontend/src/components/plot-components/TrackWindow.jsx
+++ oceannavigator/frontend/src/components/plot-components/TrackWindow.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import { Card, Col, Row } from "react-bootstrap";
 
@@ -14,309 +14,264 @@ import SelectBox from "../lib/SelectBox.jsx";
 import {
   GetDatasetsPromise,
   GetVariablesPromise,
-  GetTrackTimeRangePromise
+  GetTrackTimeRangePromise,
 } from "../../remote/OceanNavigator.js";
 
 import { withTranslation } from "react-i18next";
 
-class TrackWindow extends React.Component {
+const TrackWindow = (props) => {
+  const [showmap, setShowMap] = useState(true);
+  const [dataset, setDataset] = useState(props.dataset);
+  const [variable, setVariable] = useState(props.dataset.variable);
+  const [availableDatasets, setAvailableDatasets] = useState([]);
+  const [availableVariables, setAvailableVariables] = useState([]);
+  const [latlon, setLatlon] = useState(false);
+  const [trackvariable, setTrackVariable] = useState([0]);
+  const [starttime, setStarttime] = useState();
+  const [endtime, setEndtime] = useState();
+  const [plotSize, setPlotSize] = useState("10x7");
+  const [plotDpi, setPlotDpi] = useState(144);
+  const [depth, setDepth] = useState(0);
+  const [trackQuantum, setTrackQuantum] = useState("month");
+  const [minDate, setMinDate] = useState();
+  const [maxDate, setMaxDate] = useState();
 
-  constructor(props) {
-    super(props);
-    console.log(props.dataset)
-
-    this.state = {
-      showmap: true,
-      dataset: props.dataset,
-      variable: props.dataset.variable,
-      availableDatasets: [],
-      availableVariables: [],
-      latlon: false,
-      trackvariable: [0],
-      starttime: null,
-      endtime: null,
-      size: "10x7",
-      dpi: 72,
-      depth: 0,
-      track_quantum: 'month'
-    };
-
-    if (this.props.obs_query) {
-      let pairs = this.props.obs_query.split('&').map(x => x.split('='));
-      let dict = pairs.reduce(function (d, p) { d[p[0]] = p[1]; return d }, {});
-      this.state['starttime'] = new Date(dict.start_date);
-      this.state['endtime'] = new Date(dict.end_date);
-      this.state['track_quantum'] = dict.quantum;
+  useEffect(() => {
+    if (props.obs_query) {
+      let pairs = props.obs_query.split("&").map((x) => x.split("="));
+      let dict = pairs.reduce(function (d, p) {
+        d[p[0]] = p[1];
+        return d;
+      }, {});
+      setStarttime(new Date(dict.start_date));
+      setEndtime(new Date(dict.end_date));
+      setTrackQuantum(dict.quantum);
     }
 
-    if (props.init !== null) {
-      this.state = { ...this.state, ...props.init };
-    }
-
-    // Function bindings
-    this.onLocalUpdate = this.onLocalUpdate.bind(this);
-  }
-
-  componentDidMount() {
     GetDatasetsPromise().then((result) => {
-      this.setState({ availableDatasets: result.data })
+      setAvailableDatasets(result.data);
     });
 
-    this.getVariables(this.state.dataset.id)
+    getVariables(dataset.id);
 
-    GetTrackTimeRangePromise(this.props.track).then(
-      (result) => {
-        let starttime = this.state.starttime
-        let endtime = this.state.endtime
-        let mindate = new Date(result.data.min)
-        let maxdate = new Date(result.data.max)
+    GetTrackTimeRangePromise(props.track).then((result) => {
+      let newMindate = new Date(result.data.min);
+      let newMaxdate = new Date(result.data.max);
 
-        if (starttime < mindate) {
-          starttime = mindate
-        }
-        if (endtime > maxdate) {
-          endtime = maxdate
-        }
-
-        this.setState({
-          mindate: mindate,
-          maxdate: maxdate,
-          starttime: starttime,
-          endtime: endtime,
-        });
+      if (starttime < mindate) {
+        setStarttime(mindate);
       }
-    )
-  }
-
-  getVariables(dataset) {
-    GetVariablesPromise(dataset).then(
-      (variableResult) => {
-        let newState = {}
-        let variables = variableResult.data
-
-        newState["availableVariables"] = variables
-
-        let currentVariable = variables.filter((v) => {
-          return v.id === this.state.variable;
-        })[0];
-
-        if (!currentVariable) {
-          newState["variable"] = variables[0].id
-        }
-
-        this.setState(newState)
+      if (endtime > maxdate) {
+        setEndtime(maxdate);
       }
-    )
-  }
 
-  onLocalUpdate(key, value) {
-    var newState = {};
-    if (key == "dataset") {
-      let newDataset = this.state.availableDatasets.filter((d) => {
-        return d.id === value;
+      setMinDate(newMindate);
+      setMaxDate(newMaxdate);
+    });
+  }, []);
+
+  const getVariables = (dataset) => {
+    GetVariablesPromise(dataset).then((variableResult) => {
+      let variables = variableResult.data;
+
+      setAvailableVariables(variables);
+
+      let currentVariable = variables.filter((v) => {
+        return v.id === variable;
       })[0];
-      this.getVariables(newDataset.id)
-      newState[key] = newDataset
-    } else
-      if (typeof (key) === "string") {
-        newState[key] = value;
-      } else {
-        for (var i = 0; i < key.length; i++) {
-          newState[key[i]] = value[i];
-        }
-      }
 
-    this.setState(newState);
+      if (!currentVariable) {
+        setVariable(variables[0].id);
+      }
+    });
+  };
+
+  const onLocalUpdate = (key, value) => {
+    // var newState = {};
+    // if (key == "dataset") {
+    //   let newDataset = availableDatasets.filter((d) => {
+    //     return d.id === value;
+    //   })[0];
+    //   this.getVariables(newDataset.id);
+    //   newState[key] = newDataset;
+    // } else if (typeof key === "string") {
+    //   newState[key] = value;
+    // } else {
+    //   for (var i = 0; i < key.length; i++) {
+    //     newState[key[i]] = value[i];
+    //   }
+    // }
+
+    // this.setState(newState);
+  };
+
+  var plot_query = {
+    dataset: dataset.id,
+    quantum: dataset.quantum,
+    name: props.name,
+    type: "track",
+    track: props.track,
+    showmap: showmap,
+    variable: variable,
+    latlon: latlon,
+    trackvariable: trackvariable,
+    size: plotSize,
+    dpi: plotDpi,
+    depth: depth,
+    track_quantum: trackQuantum,
+  };
+
+  if (starttime) {
+    if (plot_query.starttime instanceof Date) {
+      plot_query.starttime = starttime.toISOString();
+      plot_query.endtime = endtime.toISOString();
+    } else {
+      plot_query.starttime = starttime;
+      plot_query.endtime = endtime;
+    }
   }
 
-  render() {
-    _("Dataset");
-    _("Observed Variable");
-    _("Variable");
-    _("Show Map");
-    _("Show Latitude/Longitude Plots");
-    _("Start Time");
-    _("End Time");
-    _("Saved Image Size");
-
-    let dataset = null;
-    let variable = null;
-    if (this.state.availableDatasets.length > 0) {
-      dataset = <DatasetDropdown
-        id="dataset"
-        key="dataset"
-        options={this.state.availableDatasets}
-        label={_("Dataset")}
-        placeholder={_("Dataset")}
-        onChange={this.onLocalUpdate}
-        selected={this.state.dataset.id}
-      />
-    }
-
-    if (this.state.availableVariables.length > 0) {
-      variable = <SelectBox
-        key="variable"
-        id="variable"
-        name={_("variable")}
-        label={_("Variable")}
-        placeholder={_("Variable")}
-        options={this.state.availableVariables}
-        onChange={this.onLocalUpdate}
-        selected={this.state.variable}
-      />
-    }
-
-    var trackvariable = <ComboBox
-      key='trackvariable'
-      id='trackvariable'
-      multiple
-      state={this.state.trackvariable}
-      def=''
-      onUpdate={this.onLocalUpdate}
-      url={`/api/v2.0/observation/variables/platform=${this.props.track[0]}.json`}
-      title={_("Observed Variable")}
-    ><h1>Track Variable</h1></ComboBox>;
-
-
-    var showmap = <CheckBox
-      key='showmap'
-      id='showmap'
-      checked={this.state.showmap}
-      onUpdate={this.onLocalUpdate}
-      title={_("Show Map")}
-    >{_("showmap_help")}</CheckBox>;
-
-    var latlon = <CheckBox
-      key='latlon'
-      id='latlon'
-      checked={this.state.latlon}
-      onUpdate={this.onLocalUpdate}
-      title={_("Show Latitude/Longitude Plots")}
-    >{_("latlon_help")}</CheckBox>;
-
-    var starttime = <div key="starttime-div">
-      <h1 className="time-label">Start Date</h1>
-      <DatePicker
-        key='starttime'
-        id="starttime"
-        dateFormat="yyyy-MM-dd"
-        selected={this.state.starttime}
-        onChange={(newDate) => this.onLocalUpdate("starttime", newDate)}
-        minDate={this.state.mindate}
-        maxDate={this.state.endtime}
-      />
-    </div>
-
-    var endtime = <div key="endtime-div">
-      <h1 className="time-label">End Date</h1>
-      <DatePicker
-        key='endtime'
-        id="endtime"
-        dateFormat="yyyy-MM-dd"
-        selected={this.state.endtime}
-        onChange={(newDate) => this.onLocalUpdate("endtime", newDate)}
-        minDate={this.state.starttime}
-        maxDate={this.state.maxdate}
-      />
-    </div>
-
-    var track_quantum = <ComboBox
-      key='track_quantum'
-      id='track_quantum'
-      state={this.state.track_quantum}
-      data={[
-        { id: 'minute', value: 'Minute' },
-        { id: 'hour', value: 'Hour' },
-        { id: 'day', value: 'Day' },
-        { id: 'week', value: 'Week' },
-        { id: 'month', value: 'Month' },
-        { id: 'year', value: 'Year' },
-      ]}
-      title='Track Simplification'
-      onUpdate={this.onLocalUpdate}
-    />;
-
-    var size = <ImageSize
-      key='size'
-      id='size'
-      onUpdate={this.onLocalUpdate}
-      title={_("Saved Image Size")}
-    />;
-
-    var accordion = <Accordion key="plot-options-accordion">
-      <Accordion.Header>Plot Options</Accordion.Header>
-      <Accordion.Body className="plot-accordion">
-        {size}
-      </Accordion.Body>
-    </Accordion>
-
-    var depth = <ComboBox
-      key='depth'
-      id='depth'
-      state={this.state.depth}
-      def={""}
-      onUpdate={this.onLocalUpdate}
-      url={`/api/v2.0/dataset/${this.props.dataset.id}/${this.state.variable}/depths?include_all_key=true`}
-      title={_("Depth")}
-    />;
-
-    var inputs = [];
-    var plot_query = {
-      dataset: this.state.dataset.id,
-      quantum: this.state.dataset.quantum,
-      name: this.props.name,
-      type: "track",
-      track: this.props.track,
-      showmap: this.state.showmap,
-      variable: this.state.variable,
-      latlon: this.state.latlon,
-      trackvariable: this.state.trackvariable,
-      size: this.state.size,
-      dpi: this.state.dpi,
-      depth: this.state.depth,
-      track_quantum: this.state.track_quantum,
-    };
-
-    if (this.state.starttime) {
-      if (plot_query.starttime instanceof Date) {
-        plot_query.starttime = this.state.starttime.toISOString();
-        plot_query.endtime = this.state.endtime.toISOString();
-      } else {
-        plot_query.starttime = this.state.starttime;
-        plot_query.endtime = this.state.endtime;
-      }
-    }
-
-    inputs = [
-      dataset, variable, showmap, latlon, starttime, endtime, track_quantum,
-      trackvariable, depth, accordion
-    ];
-
-    return (
-      <div className='TrackWindow'>
-        <Row>
-          <Col className="settings-col" lg={2}>
-            <Card variant="primary" key="map_settings">
-              <Card.Header>{_("Track Settings")}</Card.Header>
-              <Card.Body className="global-settings-card">
-                <div className="inputs-container">
-                  {inputs}
+  return (
+    <div className="TrackWindow">
+      <Row>
+        <Col className="settings-col" lg={2}>
+          <Card variant="primary" key="map_settings">
+            <Card.Header>{_("Track Settings")}</Card.Header>
+            <Card.Body className="global-settings-card">
+              <div className="inputs-container">
+                {availableDatasets.length > 0 && (
+                  <DatasetDropdown
+                    id="dataset"
+                    key="dataset"
+                    options={availableDatasets}
+                    label={_("Dataset")}
+                    placeholder={_("Dataset")}
+                    onChange={onLocalUpdate}
+                    selected={dataset.id}
+                  />
+                )}
+                {availableVariables.length > 0 && (
+                  <SelectBox
+                    key="variable"
+                    id="variable"
+                    name={_("variable")}
+                    label={_("Variable")}
+                    placeholder={_("Variable")}
+                    options={availableVariables}
+                    onChange={onLocalUpdate}
+                    selected={variable}
+                  />
+                )}
+                <ComboBox
+                  key="trackvariable"
+                  id="trackvariable"
+                  multiple
+                  state={trackvariable}
+                  def=""
+                  onUpdate={onLocalUpdate}
+                  url={`/api/v2.0/observation/variables/platform=${props.plotData.id}.json`}
+                  title={_("Observed Variable")}
+                >
+                  <h1>Track Variable</h1>
+                </ComboBox>
+                <CheckBox
+                  key="showmap"
+                  id="showmap"
+                  checked={showmap}
+                  onUpdate={onLocalUpdate}
+                  title={_("Show Map")}
+                >
+                  {_("showmap_help")}
+                </CheckBox>
+                ;
+                <CheckBox
+                  key="latlon"
+                  id="latlon"
+                  checked={latlon}
+                  onUpdate={onLocalUpdate}
+                  title={_("Show Latitude/Longitude Plots")}
+                >
+                  {_("latlon_help")}
+                </CheckBox>
+                ;
+                <div key="starttime-div">
+                  <h1 className="time-label">Start Date</h1>
+                  <DatePicker
+                    key="starttime"
+                    id="starttime"
+                    dateFormat="yyyy-MM-dd"
+                    selected={starttime}
+                    onChange={(newDate) =>
+                      onLocalUpdate("starttime", newDate)
+                    }
+                    minDate={minDate}
+                    maxDate={endtime}
+                  />
                 </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="plot-col" lg={8}>
-            <PlotImage
-              query={plot_query} // For image saving link.
-              permlink_subquery={this.state}
-              action={this.props.action}
-            />
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-}
+                <div key="endtime-div">
+                  <h1 className="time-label">End Date</h1>
+                  <DatePicker
+                    key="endtime"
+                    id="endtime"
+                    dateFormat="yyyy-MM-dd"
+                    selected={endtime}
+                    onChange={(newDate) =>
+                      onLocalUpdate("endtime", newDate)
+                    }
+                    minDate={starttime}
+                    maxDate={maxDate}
+                  />
+                </div>
+                <ComboBox
+                  key="track_quantum"
+                  id="track_quantum"
+                  state={trackQuantum}
+                  data={[
+                    { id: "minute", value: "Minute" },
+                    { id: "hour", value: "Hour" },
+                    { id: "day", value: "Day" },
+                    { id: "week", value: "Week" },
+                    { id: "month", value: "Month" },
+                    { id: "year", value: "Year" },
+                  ]}
+                  title="Track Simplification"
+                  onUpdate={onLocalUpdate}
+                />
+                <Accordion key="plot-options-accordion">
+                  <Accordion.Header>Plot Options</Accordion.Header>
+                  <Accordion.Body className="plot-accordion">
+                    <ImageSize
+                      key="size"
+                      id="size"
+                      onUpdate={onLocalUpdate}
+                      title={_("Saved Image Size")}
+                    />
+                  </Accordion.Body>
+                </Accordion>
+                <ComboBox
+                  key="depth"
+                  id="depth"
+                  state={depth}
+                  def={""}
+                  onUpdate={onLocalUpdate}
+                  url={`/api/v2.0/dataset/${props.dataset.id}/${variable}/depths?include_all_key=true`}
+                  title={_("Depth")}
+                />
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="plot-col" lg={8}>
+          <PlotImage
+            query={plot_query} // For image saving link.
+            // permlink_subquery={this.state}
+            action={props.action}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 //***********************************************************************
 TrackWindow.propTypes = {
@@ -325,7 +280,7 @@ TrackWindow.propTypes = {
   onUpdate: PropTypes.func,
   init: PropTypes.object,
   action: PropTypes.func,
-  obs_query: PropTypes.string
+  obs_query: PropTypes.string,
 };
 
 export default withTranslation()(TrackWindow);
