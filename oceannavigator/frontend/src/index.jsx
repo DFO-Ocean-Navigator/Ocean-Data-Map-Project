8ba3ffe9a4272dccce82ab704ae48bcc9195dea6
JustinElms <justinwelms@gmail.com>
2025-12-15 15:20:08 +0000

    add debug tools

--- oceannavigator/frontend/src/index.jsx
+++ oceannavigator/frontend/src/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import OceanNavigator from "./components/OceanNavigator.jsx";
 import ReactGA from "react-ga";
 
@@ -10,15 +12,21 @@ require("./stylesheets/utils/bootstrap.css");
 require("./stylesheets/main.scss");
 
 if (process.env.NODE_ENV == "production") {
-    ReactGA.initialize("UA-122671965-2");
-  } 
-  else {
-    ReactGA.initialize("UA-122671965-3");
-  }
-  
-  ReactGA.pageview(window.location.pathname + window.location.search);
-  
+  ReactGA.initialize("UA-122671965-2");
+} else {
+  ReactGA.initialize("UA-122671965-3");
+}
+
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 document.title = "Ocean Navigator";
 
+const queryClient = new QueryClient();
+
 const root = createRoot(document.getElementById("app"));
-root.render(<OceanNavigator />);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <OceanNavigator />
+    <ReactQueryDevtools initialIsOpen={true} />
+  </QueryClientProvider>
+);
