acc869b2d8b564a2d1f8a6c339c1916442e6ce42
Justin Elms <79917349+JustinElms@users.noreply.github.com>
2026-03-20 14:19:15 -0230

    Update Sentry configuration (#1290)
    * add sentry react error reporting
    
    * update sentry python config

--- oceannavigator/frontend/src/index.jsx
+++ oceannavigator/frontend/src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import OceanNavigator from "./components/OceanNavigator.jsx";
 import ReactGA from "react-ga";
@@ -18,13 +19,22 @@ if (process.env.NODE_ENV == "production") {
 
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-document.title = "Ocean Navigator";
+const SENTRY_ENV = process.env.ONAV_SENTRY_ENV;
+const SENTRY_DSN = process.env.ONAV_SENTRY_JS_DSN;
+const SENTRY_TRACES_RATE = process.env.ONAV_SENTRY_TRACES_RATE;
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  tracesSampleRate: SENTRY_TRACES_RATE,
+  environment: SENTRY_ENV,
+});
 
 const queryClient = new QueryClient();
 
+document.title = "Ocean Navigator";
 const root = createRoot(document.getElementById("app"));
 root.render(
   <QueryClientProvider client={queryClient}>
     <OceanNavigator />
-  </QueryClientProvider>
+  </QueryClientProvider>,
 );
