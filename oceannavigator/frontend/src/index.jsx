b90cebd5024f6abc6a6151472610677eca05f004
JustinElms <justinwelms@gmail.com>
2026-03-26 12:47:54 +0000

    remove duplicate pkg import

--- oceannavigator/frontend/src/index.jsx
+++ oceannavigator/frontend/src/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import * as Sentry from "@sentry/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import OceanNavigator from "./components/OceanNavigator.jsx";
 import ReactGA from "react-ga";
